25d85572758235404b7e492ac5227ce333c3ffb4
Georg Lang <mail@georglang.com>
2018-10-03 10:25:36 +0200

    fetching data from indexDB to material table in record-list

--- src/app/record-list/record-list.component.ts
+++ src/app/record-list/record-list.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, RouterModule, Routes, ActivatedRoute, ParamMap } from '@angular/router';
+import { IndexDBService } from '../database/index-db.service';
+
+export interface ICustomerRecord {
+  name: string;
+  place: string;
+}
 
 @Component({
   selector: 'app-record-list',
@@ -7,8 +13,28 @@ import { Router, RouterModule, Routes, ActivatedRoute, ParamMap } from '@angular
   styleUrls: ['./record-list.component.sass']
 })
 export class RecordListComponent implements OnInit {
-  public customers = ['Lang', 'Huber', 'Maier', 'Müller'];
-  constructor(private route: ActivatedRoute, private router: Router) {}
+  public dataSource: ICustomerRecord[];
+  public CustomerRecords: ICustomerRecord[] = [];
+  public displayedColumns: string[] = ['name', 'place'];
 
-  ngOnInit() {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private indexDbService: IndexDBService
+  ) {}
+
+  getCustomerNameAndPlace() {
+    this.indexDbService.getAllRecords().then(allRecords => {
+      allRecords.forEach(record => {
+        if ('place' in record) {
+          this.CustomerRecords.push({ name: record.customer, place: record.place });
+        }
+        this.dataSource = this.CustomerRecords;
+      });
+    });
+  }
+
+  ngOnInit() {
+    this.getCustomerNameAndPlace();
+  }
 }
