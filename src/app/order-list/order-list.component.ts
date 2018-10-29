871977221bd5aa61f466302d1bd08497736df52c
Georg Lang <mail@georglang.com>
2018-10-29 21:16:06 +0100

    searchable table included

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
 import { IndexDBService } from '../service/index-db.service';
 import { Order } from './../data-classes/order';
 
@@ -8,17 +9,29 @@ import { Order } from './../data-classes/order';
   styleUrls: ['./order-list.component.sass']
 })
 export class OrderListComponent implements OnInit {
-  public orders: Order[];
-  public OrderRecords: Order[] = [];
+  public orders: Order[] = [];
+  public dataSource = new MatTableDataSource();
+  public displayedColumns = ['customer', 'contactPerson', 'location'];
+
+  @ViewChild(MatSort)
+  sort: MatSort;
 
   constructor(private indexDbService: IndexDBService) {}
+
+  public applyFilter(filterValue: string) {
+    filterValue = filterValue.trim();
+    filterValue = filterValue.toLocaleLowerCase();
+    this.dataSource.filter = filterValue;
+  }
 
   getOrders() {
     this.indexDbService.getAllOrders().then(allOrders => {
       allOrders.forEach(order => {
-        this.OrderRecords.push(new Order(order.companyName, order.location, order.id, order.contactPerson));
+        this.orders.push(
+          new Order(order.companyName, order.location, order.id, order.contactPerson)
+        );
       });
-      this.orders = this.OrderRecords;
+      this.dataSource = new MatTableDataSource(this.orders);
     });
   }
 
