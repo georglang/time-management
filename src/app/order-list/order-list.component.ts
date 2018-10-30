a2e8fbfbd406ce850484c164aed7a57893fb4e06
Georg Lang <mail@georglang.com>
2018-10-30 19:10:36 +0100

    new design and routing

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 import { MatSort, MatTableDataSource } from '@angular/material';
 import { IndexDBService } from '../service/index-db.service';
 import { Order } from './../data-classes/order';
@@ -11,12 +12,12 @@ import { Order } from './../data-classes/order';
 export class OrderListComponent implements OnInit {
   public orders: Order[] = [];
   public dataSource = new MatTableDataSource();
-  public displayedColumns = ['customer', 'contactPerson', 'location'];
+  public displayedColumns = ['customer', 'contactPerson', 'location', 'icon'];
 
   @ViewChild(MatSort)
   sort: MatSort;
 
-  constructor(private indexDbService: IndexDBService) {}
+  constructor(private indexDbService: IndexDBService, private router: Router) {}
 
   public applyFilter(filterValue: string) {
     filterValue = filterValue.trim();
@@ -24,11 +25,25 @@ export class OrderListComponent implements OnInit {
     this.dataSource.filter = filterValue;
   }
 
+  getRecord(row: any) {
+    console.log('Row', row.id);
+    this.router.navigate(['/order-details/' + row.id]);
+  }
+
+  createOrder() {
+    this.router.navigate(['/create-order']);
+  }
+
   getOrders() {
     this.indexDbService.getAllOrders().then(allOrders => {
       allOrders.forEach(order => {
         this.orders.push(
-          new Order(order.companyName, order.location, order.id, order.contactPerson)
+          new Order(
+            order.companyName,
+            order.location,
+            order.id,
+            order.contactPerson
+          )
         );
       });
       this.dataSource = new MatTableDataSource(this.orders);
