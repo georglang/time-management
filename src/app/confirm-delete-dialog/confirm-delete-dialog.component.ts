95ba134b1bb8f750697dee5f3e7a3fff8079e693
Georg Lang <mail@georglang.com>
2018-11-13 20:54:13 +0100

    confirm delete dialog style path fixed

--- src/app/confirm-delete-dialog/confirm-delete-dialog.component.ts
+++ src/app/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -4,7 +4,7 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 @Component({
   selector: 'app-confirm-delete-dialog',
   templateUrl: './confirm-delete-dialog.component.html',
-  styleUrls: [' ./confirm-delete-dialog.component.sass']
+  styleUrls: ['./confirm-delete-dialog.component.sass']
 })
 export class ConfirmDeleteDialogComponent implements OnInit {
   public modalTitle: string;
