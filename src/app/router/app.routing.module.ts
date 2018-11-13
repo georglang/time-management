2273b5c710e13c92644738e24bf66b6f0170c861
Georg Lang <mail@georglang.com>
2018-11-13 16:04:18 +0100

    fixed routing to edit-record

--- src/app/router/app.routing.module.ts
+++ src/app/router/app.routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, ActivatedRoute, ParamMap  } from '@angular/router';
+import { RouterModule, Routes, ActivatedRoute, ParamMap } from '@angular/router';
 import { OrderListComponent } from '../order-list/order-list.component';
 import { CreateOrderComponent } from '../create-order/create-order.component';
 import { OrderDetailComponent } from '../order-detail/order-detail.component';
@@ -13,25 +13,15 @@ const appRoutes = [
   {
     path: 'order-details/:id',
     children: [
-      { path: 'detail', component: OrderDetailComponent},
+      { path: '', component: OrderDetailComponent },
       { path: 'create-record', component: CreateRecordComponent },
-      { path: 'edit-record/:id', component: EditRecordComponent},
+      { path: 'edit-record/:id', component: EditRecordComponent }
     ]
   },
   // * wildcard if the requested URL doesn´t match any path in the URL
   // could also be a 404 page
-  //{ path: '**', component: OrderListComponent, pathMatch: 'full' }
+  { path: '**', component: OrderListComponent, pathMatch: 'full' }
 ];
-
-// {
-//   path: 'machine/:machineId',
-//   component: MachineDetailsComponent,
-//   children: [
-//     { path: '', redirectTo: 'overview', pathMatch: 'full' },
-//     { path: 'overview', component: OverviewComponent }
-//     /*       {path: 'acceptance-report', component: AcceptanceReportComponent},*/
-//   ]
-// },
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
