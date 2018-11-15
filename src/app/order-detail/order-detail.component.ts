1c712bd067507e9a7aac02bd383a08eb01a1b2fa
Georg Lang <mail@georglang.com>
2018-11-15 22:31:18 +0100

    create pdfs of records as a table

--- src/app/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { MatTableDataSource, MatDialog, MatDialogConfig } from '@angular/material';
@@ -10,6 +10,15 @@ import { IOrder } from '../data-classes/order';
 import { ConfirmDeleteDialogComponent } from './../confirm-delete-dialog/confirm-delete-dialog.component';
 import { ToastrService, Toast } from 'ngx-toastr';
 
+// import * as jsPDF from 'jspdf';
+// import * from 'jspdf-autotable';
+import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
+// declare let jsPDF;
+
+
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-order-detail',
   templateUrl: './order-detail.component.html',
@@ -18,6 +27,7 @@ import { ToastrService, Toast } from 'ngx-toastr';
 export class OrderDetailComponent implements OnInit {
   private paramId;
   private sub;
+  @Input() customer;
 
   public timeRecordForm: FormGroup;
   public createRecordForm: FormGroup;
@@ -28,6 +38,7 @@ export class OrderDetailComponent implements OnInit {
   public lastId: number;
   public displayedColumns = ['id', 'date', 'description', 'workingHours', 'action'];
   public dataSource: MatTableDataSource<ITimeRecord>;
+  private pdf: jsPDF;
 
   public form_validation_messages = {
     description: [{ type: 'required', message: 'Bitte Art der Arbeit eintragen' }],
@@ -41,7 +52,8 @@ export class OrderDetailComponent implements OnInit {
     private dateAdapter: DateAdapter<Date>,
     private indexDbService: IndexDBService,
     private toastrService: ToastrService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private customerService: CustomerService
   ) {
     this.dateAdapter.setLocale('de');
     this.columns = ['Date', 'Description', 'Time', 'Delete'];
@@ -201,13 +213,13 @@ export class OrderDetailComponent implements OnInit {
     this.toastrService.success('Erfolgreich erstellt', 'Eintrag', successConfig);
   }
 
-  openConfirmDialog(recordId) {
+  public openConfirmDialog(recordId) {
     const dialogConfig = new MatDialogConfig();
     dialogConfig.disableClose = true;
     dialogConfig.autoFocus = true;
     dialogConfig.data = {
       id: 1,
-      title: 'Eintrag Löschen'
+      title: 'Löschen'
     };
     const dialogRef = this.dialog.open(ConfirmDeleteDialogComponent, dialogConfig);
     dialogRef.afterClosed().subscribe(result => {
@@ -224,5 +236,54 @@ export class OrderDetailComponent implements OnInit {
       console.log('Dialog geschlossen');
       console.log('RESULT: ', result);
     });
+  }
+
+  public print() {
+
+    const pdf = new jsPDF('p', 'pt', 'a4');
+    const margins = {
+      top: 70,
+      bottom: 40,
+      left: 30,
+      width: 550
+    };
+
+    pdf.setFontSize(18);
+    const columns = [
+      { title: 'Datum', dataKey: 'date' },
+      { title: 'Beschreibung', dataKey: 'description' },
+      { title: 'Arbeitsstunden', dataKey: 'workingHours' },
+    ];
+
+    const recordsToPrint = this.records;
+
+    recordsToPrint.forEach((record) => {
+      delete record['id'];
+      const date = record['date'];
+      const formattedDate = moment(date).format('DD.MM.YYYY');
+      record.date = formattedDate;
+    });
+
+    pdf.autoTable(columns, recordsToPrint, {
+      bodyStyles: { valign: 'top'},
+      styles: { overflow: 'linebreak', columnWidth: 'wrap'},
+      columnStyles: {
+        description: {columnWidth: 'auto'}
+      },
+      margin: {top: 60, left: 30},
+  });
+
+    pdf.fromHTML(document.getElementById('customer-info'),
+      margins.left, // x coord
+      margins.top,
+      {
+        width: margins.width// max width of content on PDF
+      });
+
+    const iframe = document.createElement('iframe');
+    iframe.setAttribute('style','position:absolute;right:0; top:0; bottom:0; height:100%; width:650px; padding:20px;');
+    document.body.appendChild(iframe);
+
+    iframe.src = pdf.output('datauristring');
   }
 }
