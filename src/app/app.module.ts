9de0458399ab25deb3f1c84b410493793cd76672
Georg Lang <mail@georglang.com>
2018-12-25 11:05:05 +0100

    changed firebase dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
 import { environment } from '../environments/environment';
 
+// firebase modulese
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
 // own modules
-import {AppRoutingModule} from './router/app.routing.module';
+import { AppRoutingModule } from './router/app.routing.module';
 import { ToastrModule } from 'ngx-toastr';
 
 // pipes
 import { SortFormArrayByDate } from './pipes/sort-form-array-by-date.pipe';
-
 
 // Angular Material
 import {
@@ -44,7 +46,7 @@ import { EditRecordComponent } from './edit-record/edit-record.component';
 import { CreateRecordComponent } from './create-record/create-record.component';
 import { ConfirmDeleteDialogComponent } from './confirm-delete-dialog/confirm-delete-dialog.component';
 
-const firebaseConfig = environment.firebaseConfig;
+const firebaseConfig = environment.firebase;
 
 @NgModule({
   declarations: [
@@ -79,16 +81,11 @@ const firebaseConfig = environment.firebaseConfig;
     MatDialogModule,
     ToastrModule.forRoot(),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule.enablePersistence()  ],
-  providers: [
-    IndexDBService,
-    Database,
-    SortFormArrayByDate,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
-  entryComponents: [
-    ConfirmDeleteDialogComponent
-  ],
+  providers: [IndexDBService, Database, SortFormArrayByDate],
+  entryComponents: [ConfirmDeleteDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
