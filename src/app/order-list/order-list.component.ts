d9662be379d95a72a609f64de4b035b01fcd7fbe
Georg Lang <mail@georglang.com>
2019-01-03 20:29:43 +0100

    removed unnecessary cloudFirestoreService.getRecords by id and cleanup

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 import { MatSort, MatTableDataSource } from '@angular/material';
 import { IndexDBService } from '../service/index-db.service';
-import { IOrder, Order } from './../data-classes/order';
+import { IOrder } from './../data-classes/order';
 import { CloudFirestoreService } from '../service/cloud-firestore.service';
 import { ConnectionService } from 'ng-connection-service';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators/';
 
 @Component({
   selector: 'app-order-list',
@@ -14,7 +13,7 @@ import { map } from 'rxjs/operators/';
   styleUrls: ['./order-list.component.sass']
 })
 export class OrderListComponent implements OnInit {
-  public orders: IOrder[];
+  public orders: any[]; // because of firebase auto generated id, IOrder coudn´t be used
   public dataSource = new MatTableDataSource();
   public displayedColumns = ['customer', 'contactPerson', 'location', 'icon'];
   public isOnline: boolean;
@@ -27,18 +26,15 @@ export class OrderListComponent implements OnInit {
   constructor(
     private indexDbService: IndexDBService,
     private router: Router,
-    private route: ActivatedRoute,
     private cloudFirestoreService: CloudFirestoreService,
     private connectionService: ConnectionService
   ) {}
 
   ngOnInit() {
-    const orderIds = [];
-
-    this.cloudFirestoreService.getCompleteData();
+    const orderIds: any = [];
 
     // if state changes from offline to online synchronize ordersOutbox with server
-    this.connectionService.monitor().subscribe(isConnected => {
+    this.connectionService.monitor().subscribe(() => {
       this.indexDbService.getOrdersFromOutbox().then(orders => {
         if (orders.length !== 0) {
           orders.forEach(order => {
@@ -52,33 +48,30 @@ export class OrderListComponent implements OnInit {
       });
     });
 
-    // If internet connection persists, get data from firebase instead from indexedDB
+    // if internet connection persists, get data from firebase and save it to indexedDB
+    // the firebase unique id will be used as indexedDB key
     if (this.isConnected()) {
       this.ordersObs = this.cloudFirestoreService.getOrders();
       this.ordersObs.subscribe(orders => {
         this.orders = orders;
         this.orders.forEach(order => {
-          this.cloudFirestoreService.getRecords(order.id).subscribe(records => {
-            order.records = records;
-            if (this.orders.length !== 0) {
-              this.indexDbService.getAllOrders().then(ordersInIndexedDB => {
-                ordersInIndexedDB.forEach(cachedOrder => {
-                  orderIds.push(cachedOrder.id);
-                });
-
-                this.orders.forEach(_order => {
-                  if (!orderIds.includes(_order.id)) {
-                    this.indexDbService.addOrder(order);
-                  }
-                });
+          if (this.orders.length !== 0) {
+            this.indexDbService.getAllOrders().then(ordersInIndexedDB => {
+              ordersInIndexedDB.forEach(cachedOrder => {
+                orderIds.push(cachedOrder.id);
               });
-            }
-            this.dataSource = new MatTableDataSource(this.orders);
-          });
+              this.orders.forEach(_order => {
+                if (!orderIds.includes(_order.id)) {
+                  this.indexDbService.addOrder(order);
+                }
+              });
+            });
+          }
+          this.dataSource = new MatTableDataSource(this.orders);
         });
       });
     } else {
-      console.log('No internet connection');
+      console.error('No internet connection');
       this.indexDbService.getAllOrders().then(data => {
         this.dataSource = new MatTableDataSource(data);
       });
