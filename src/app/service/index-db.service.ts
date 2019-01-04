fede36075ba1d785f920817d0f4389edc220a6de
Georg Lang <mail@georglang.com>
2019-01-04 20:26:48 +0100

    fixed addRecordToOrder and commented methods out for debugging purpose

--- src/app/service/index-db.service.ts
+++ src/app/service/index-db.service.ts
@@ -7,9 +7,7 @@ import { CloudFirestoreService } from './cloud-firestore.service';
 
 @Injectable()
 export class IndexDBService {
-  private records;
   public isAlreadyInDB: boolean;
-  private isOnline;
 
   constructor(private timeRecordsDb: Database, private cloudFirestore: CloudFirestoreService) {
     this.isAlreadyInDB = false;
@@ -19,77 +17,84 @@ export class IndexDBService {
     return navigator.onLine;
   }
 
-  public createUniqueId() {
-    const ID = () => {
-      const array = new Uint32Array(8);
-      window.crypto.getRandomValues(array);
-      let str = '';
-      for (let i = 0; i < array.length; i++) {
-        str += (i < 2 || i > 5 ? '' : '-') + array[i].toString(16).slice(-4);
-      }
-      return str;
-    };
-    return ID();
-  }
-
   public addRecordToOrder(record, orderId) {
-    console.log('Add record To Order ++++', record, orderId);
-    console.log('IS ONLINE', this.isConnected());
+    return this.timeRecordsDb.orders
+        .where('id')
+        .equals(orderId)
+        .toArray(orders => {
+          if (orders !== undefined) {
+            const records = orders[0].records;
+            // if the order has no record
+            if (records === undefined) {
+              orders[0].records = [];
+              orders[0].records.push(record);
+            } else {
+              if (record.id === undefined) {
+                orders[0].records.push(record);
+              }
+              this.timeRecordsDb.orders
+              .where('id')
+              .equals(orderId)
+              .modify(orders[0]);
+            }
+          }
+        });
 
     if (this.isConnected()) {
-      return this.timeRecordsDb.orders
-        .where('id')
-        .equals(orderId)
-        .toArray(orders => {
-          console.log('TOO ARRAy', orders);
 
-          if (orders !== undefined) {
-            const records = orders[0].records;
-            console.log('ordersss 0', orders);
-            if (records.length === 0) {
-              orders[0].records.push(record);
-              console.log('IN IF' );
+    //   return this.timeRecordsDb.orders
+    //     .where('id')
+    //     .equals(orderId)
+    //     .toArray(orders => {
+    //       console.log('TOO ARRAy', orders);
 
-            } else {
-              console.log('IN ELSE' );
+    //       if (orders !== undefined) {
+    //         const records = orders[0].records;
+    //         console.log('ordersss 0', orders);
+    //         if (records.length === 0) {
+    //           orders[0].records.push(record);
+    //           console.log('IN IF' );
 
-              for (let i = 0; i < records.length; i++) {
-                if (record.id === records[i].id) {
-                  this.isAlreadyInDB = true;
-                }
-              }
-              if (!this.isAlreadyInDB) {
-                orders[0].records.push(record);
-              }
-            }
-            this.cloudFirestore.updateRecord(orderId, orders[0].records);
-          }
-        });
-    } else {
-      // add to Outbox
-      return this.timeRecordsDb.outboxForOrders
-        .where('id')
-        .equals(orderId)
-        .toArray(orders => {
-          if (orders !== undefined) {
-            const records = orders[0].records;
-            if (records.length === 0) {
-              orders[0].records.push(record);
-            } else {
-              for (let i = 0; i < records.length; i++) {
-                if (record.id === records[i].id) {
-                  this.isAlreadyInDB = true;
-                }
-              }
-              if (!this.isAlreadyInDB) {
-                orders[0].records.push(record);
-              }
-            }
-            this.cloudFirestore.updateRecord(orderId, orders[0].records);
-          }
+    //         } else {
+    //           console.log('IN ELSE' );
 
-        });
-    }
+    //           for (let i = 0; i < records.length; i++) {
+    //             if (record.id === records[i].id) {
+    //               this.isAlreadyInDB = true;
+    //             }
+    //           }
+    //           if (!this.isAlreadyInDB) {
+    //             orders[0].records.push(record);
+    //           }
+    //         }
+    //         this.cloudFirestore.updateRecord(orderId, orders[0].records);
+    //       }
+    //     });
+    // } else {
+    //   add to Outbox
+    //   return this.timeRecordsDb.outboxForOrders
+    //     .where('id')
+    //     .equals(orderId)
+    //     .toArray(orders => {
+    //       if (orders !== undefined) {
+    //         const records = orders[0].records;
+    //         if (records.length === 0) {
+    //           orders[0].records.push(record);
+    //         } else {
+    //           for (let i = 0; i < records.length; i++) {
+    //             if (record.id === records[i].id) {
+    //               this.isAlreadyInDB = true;
+    //             }
+    //           }
+    //           if (!this.isAlreadyInDB) {
+    //             orders[0].records.push(record);
+    //           }
+    //         }
+    //         this.cloudFirestore.updateRecord(orderId, orders[0].records);
+    //       }
+
+    //     });
+    // }
   }
 
   public getOrderByIdFromOutbox(paramId: number) {
@@ -158,54 +163,54 @@ export class IndexDBService {
       });
   }
 
-  public modifyOrder(orderId, record) {
-    return this.timeRecordsDb.orders
-      .where('id')
-      .equals(orderId)
-      .toArray(order => {
-        const records = order[0].records;
-        if (records.length === 0) {
-          order[0].records.push(record);
-        }
-        for (let index = 0; index < records.length; index++) {
-          const element = records[index];
-          if (record.id === element.id) {
-            if (!_.isEqual(record, element)) {
-              records.splice(index, 1);
-              order[0].records.push(record);
-              this.timeRecordsDb.orders
-                .where('id')
-                .equals(orderId)
-                .modify((value, ref) => {
-                  ref.value.records = records;
-                });
-            }
-          }
-        }
-      });
-  }
+  // public modifyOrder(orderId, record) {
+  //   return this.timeRecordsDb.orders
+  //     .where('id')
+  //     .equals(orderId)
+  //     .toArray(order => {
+  //       const records = order[0].records;
+  //       if (records.length === 0) {
+  //         order[0].records.push(record);
+  //       }
+  //       for (let index = 0; index < records.length; index++) {
+  //         const element = records[index];
+  //         if (record.id === element.id) {
+  //           if (!_.isEqual(record, element)) {
+  //             records.splice(index, 1);
+  //             order[0].records.push(record);
+  //             this.timeRecordsDb.orders
+  //               .where('id')
+  //               .equals(orderId)
+  //               .modify((value, ref) => {
+  //                 ref.value.records = records;
+  //               });
+  //           }
+  //         }
+  //       }
+  //     });
+  // }
 
-  public deleteRecord(orderId, recordId) {
-    return this.timeRecordsDb.orders
-      .where('id')
-      .equals(orderId)
-      .toArray(order => {
-        const records: TimeRecord[] = order[0].records;
-        for (let index = 0; index < records.length; index++) {
-          const element = records[index];
-          if (element.id === recordId) {
-            records.splice(index, 1);
-            this.timeRecordsDb.orders
-              .where('id')
-              .equals(orderId)
-              .modify(_order => {
-                _order.records = records;
-                this.cloudFirestore.updateRecord(orderId, _order.records);
-              });
-          }
-        }
-      });
-  }
+  // public deleteRecord(orderId, recordId) {
+  //   return this.timeRecordsDb.orders
+  //     .where('id')
+  //     .equals(orderId)
+  //     .toArray(order => {
+  //       const records: TimeRecord[] = order[0].records;
+  //       for (let index = 0; index < records.length; index++) {
+  //         const element = records[index];
+  //         if (element.id === recordId) {
+  //           records.splice(index, 1);
+  //           this.timeRecordsDb.orders
+  //             .where('id')
+  //             .equals(orderId)
+  //             .modify(_order => {
+  //               _order.records = records;
+  //               this.cloudFirestore.updateRecord(orderId, _order.records);
+  //             });
+  //         }
+  //       }
+  //     });
+  // }
 
   public getAllRecords(orderId) {
     return this.timeRecordsDb.orders
@@ -219,33 +224,33 @@ export class IndexDBService {
       });
   }
 
-  public updateRecord(updatedRecord: TimeRecord, orderId: string) {
-    return this.timeRecordsDb.orders
-      .where('id')
-      .equals(orderId)
-      .toArray(order => {
-        const records = order[0].records;
-        for (let index = 0; index < records.length; index++) {
-          const element = records[index];
-          if (updatedRecord.id === element.id) {
-            if (!_.isEqual(updatedRecord, element)) {
-              records.splice(index, 1);
-              order[0].records.push(updatedRecord);
-              this.timeRecordsDb.orders
-                .where('id')
-                .equals(orderId)
-                .modify((value, ref) => {
-                  ref.value.records = records;
+  // public updateRecord(updatedRecord: TimeRecord, orderId: string) {
+  //   return this.timeRecordsDb.orders
+  //     .where('id')
+  //     .equals(orderId)
+  //     .toArray(order => {
+  //       const records = order[0].records;
+  //       for (let index = 0; index < records.length; index++) {
+  //         const element = records[index];
+  //         if (updatedRecord.id === element.id) {
+  //           if (!_.isEqual(updatedRecord, element)) {
+  //             records.splice(index, 1);
+  //             order[0].records.push(updatedRecord);
+  //             this.timeRecordsDb.orders
+  //               .where('id')
+  //               .equals(orderId)
+  //               .modify((value, ref) => {
+  //                 ref.value.records = records;
 
-                  this.cloudFirestore
-                    .updateRecord(orderId.toString(), order[0].records);
-                    // .then(data => {
-                    //   console.log('Updated in firestore');
-                    // });
-                });
-            }
-          }
-        }
-      });
-  }
+  //                 this.cloudFirestore
+  //                   .updateRecord(orderId.toString(), order[0].records);
+  //                   .then(data => {
+  //                     console.log('Updated in firestore');
+  //                   });
+  //               });
+  //           }
+  //         }
+  //       }
+  //     });
+  // }
 }
