6362d2cdaf1f925d47fb1b4a9098eebe59624300
Georg Lang <mail@georglang.com>
2019-02-02 11:26:56 +0100

    fixed production environment for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA6LpeZ-_Rx-NH3NJ8bQMNKu-4eVa4LXlI',
+    authDomain: 'angular-pwa-98394.firebaseapp.com',
+    projectId: 'angular-pwa-98394',
+    databaseURL: 'https://angular-pwa-98394.firebaseio.com',
+    storageBucket: 'angular-pwa-98394.appspot.com',
+    messagingSenderId: '701351415969'
+  }
 };
