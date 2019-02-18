3d1b4b59ef3d914ad415bcfb3de44826732c6314
Georg Lang <mail@georglang.com>
2019-02-18 19:46:11 +0100

    modified synchronisation in order-list when network connection switches from offline to online

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -15,10 +15,11 @@ import { Observable } from 'rxjs';
 export class OrderListComponent implements OnInit {
   public orders: any[]; // IOrder coudn´t be used because of firebase auto generated id,
   public dataSource = new MatTableDataSource();
-  public displayedColumns = ['customer', 'contactPerson', 'location', 'icon'];
-  public isOnline: boolean;
+  public displayedColumns = ['customer', 'contactPerson', 'location', 'detail'];
+  public isOnlineService: boolean;
   private ordersFromIndexedDB: any = [];
   private ordersObs: Observable<IOrder[]>;
+  private orderIds: number[] = [];
 
   @ViewChild(MatSort)
   sort: MatSort;
@@ -31,70 +32,113 @@ export class OrderListComponent implements OnInit {
   ) {}
 
   ngOnInit() {
+    // --> als naechstes machen
+
+    // ToDo
+    // Testen
+    // Online -> speichern in firebase
+    // Offline -> speichern in outbox
+    // Offline - Online -> von outbox nach firebase und indexedDB orders speichern schauen ob bereits in indexedDB orders und firbase
+
     // if state changes from offline to online synchronize ordersOutbox with server
     // and delete orders in outbox
+
     this.connectionService.monitor().subscribe(isConnected => {
-      this.isOnline = isConnected;
-      if (this.isOnline) {
+      this.isOnlineService = isConnected;
+      if (this.isOnlineService) {
         this.indexDbService.getOrdersFromOutbox().then(orders => {
           if (orders.length !== 0) {
             orders.forEach(order => {
               const id = order.id;
               delete order.id;
               this.cloudFirestoreService.addOrder(order).then(() => {
-                this.indexDbService.deleteOrderFromOutbox(id);
+                this.indexDbService.ordersInOrdersTable().then(ordersInIndexedDB => {
+                  if (ordersInIndexedDB.length > 0) {
+                    ordersInIndexedDB.forEach(cachedOrder => {
+                      this.orderIds.push(cachedOrder.id);
+                    });
+                    orders.forEach(_order => {
+                      if (!this.orderIds.includes(_order.id)) {
+                        this.indexDbService.addToOrdersTable(order).then(() => {
+                          this.indexDbService.deleteOrderFromOutbox(id)
+                            .then(() => {
+                              this.orderIds = [];
+                            });
+                        });
+                      }
+                    });
+                  } else {
+                    this.indexDbService.addToOrdersTable(order).then(() => {
+                      this.indexDbService.deleteOrderFromOutbox(id);
+                    });
+                  }
+                });
               });
             });
           }
         });
       }
     });
-    this.getOrders();
+
+    this.getOrdersFromIndexedDB();
+
+    // if (this.isOnline()) {
+    //   this.getOrdersOnline();
+    // } else {
+    //   console.warn('No internet connection');
+    //   this.getOrdersFromIndexedDB();
+    // }
+  }
+
+  public isOnline() {
+    return navigator.onLine;
   }
 
   // if internet connection persists, get data from firebase and save it to indexedDB
   // the firebase unique id will be used as indexedDB key
-  public getOrders() {
-    const orderIds: string[] = [];
+  public getOrdersOnline() {
     if (this.isOnline) {
       this.ordersObs = this.cloudFirestoreService.getOrders();
       this.ordersObs.subscribe(orders => {
         this.orders = orders;
-        this.orders.forEach(order => {
-          if (this.orders.length !== 0) {
-            this.indexDbService.getAllOrders().then(ordersInIndexedDB => {
+        if (this.orders.length !== 0) {
+          this.orders.forEach(order => {
+            this.indexDbService.ordersInOrdersTable().then(ordersInIndexedDB => {
               ordersInIndexedDB.forEach(cachedOrder => {
-                orderIds.push(cachedOrder.id);
+                this.orderIds.push(cachedOrder.id);
               });
               this.orders.forEach(_order => {
-                if (!orderIds.includes(_order.id)) {
-                  this.indexDbService.addOrder(order);
+                if (!this.orderIds.includes(_order.id)) {
+                  this.indexDbService.addToOrdersTable(order);
                 }
               });
             });
-          }
-          this.dataSource = new MatTableDataSource(this.orders);
-        });
+            this.dataSource = new MatTableDataSource(this.orders);
+          });
+        }
       });
-    } else {
-      console.warn('No internet connection');
-      // data from indexedDB orders and outboxForOrders tables will be fetched
-      this.indexDbService
-        .getAllOrders()
-        .then(ordersTable => {
-          ordersTable.forEach(order => {
+    }
+  }
+
+  // if no internet connection persists, data from indexedDB orders and outboxForOrders tables will be fetched
+  public getOrdersFromIndexedDB() {
+    // if (!this.isOnline()) {
+    this.indexDbService
+      .ordersInOrdersTable()
+      .then(ordersTable => {
+        ordersTable.forEach(order => {
+          this.ordersFromIndexedDB.push(order);
+        });
+      })
+      .then(() => {
+        this.indexDbService.getOrdersFromOutbox().then(ordersOutboxTable => {
+          ordersOutboxTable.forEach(order => {
             this.ordersFromIndexedDB.push(order);
           });
-        })
-        .then(() => {
-          this.indexDbService.getOrdersFromOutbox().then(ordersOutboxTable => {
-            ordersOutboxTable.forEach(order => {
-              this.ordersFromIndexedDB.push(order);
-            });
-            this.dataSource = new MatTableDataSource(this.ordersFromIndexedDB);
-          });
+          this.dataSource = new MatTableDataSource(this.ordersFromIndexedDB);
         });
-    }
+      });
+    // }
   }
 
   public applyFilter(filterValue: string) {
