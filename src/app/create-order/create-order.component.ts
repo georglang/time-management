89466be3605c4a730fbe6edaf0aec8588c691174
Georg Lang <mail@georglang.com>
2019-02-24 07:41:17 +0100

    checkIfOrderIsIndexedDBOrdersTable() and checkIfOrderIsInIndexedDBOrdersOutboxTable() implemented as promise

--- src/app/create-order/create-order.component.ts
+++ src/app/create-order/create-order.component.ts
@@ -17,14 +17,11 @@ export class CreateOrderComponent implements OnInit {
   public createOrderForm: FormGroup;
   public columns: string[];
   private newOrder: IOrder;
-  private isAlreadyInOutbox = true;
   private isAlreadyInFirestore = false;
   private ordersOnline: IOrder[] = [];
   private tempOrders: any[] = [];
-  private orderIds: number[] = [];
   public isOnline: boolean;
   public orders: any[]; // IOrder coudn´t be used because of firebase auto generated id,
-  // private ordersToPushToFirebase: IOrder[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
@@ -59,6 +56,8 @@ export class CreateOrderComponent implements OnInit {
     // Wenn keine Orders in Datenbank, kann Outbox direkt gepushed werden
 
     // Wechsel von Offline nach Online, schauen ob orders in ordersOutbox
+
+    // wenn mit server synchronisiert wird, muss indexedDB id durch firebase id ersetzt werden
 
     this.indexDbService.getOrdersFromOutbox().then(ordersInOutbox => {
       if (ordersInOutbox.length !== 0) {
@@ -118,7 +117,6 @@ export class CreateOrderComponent implements OnInit {
   public sychronizeOutboxWithDatabase(ordersToPushToFirebase) {
     ordersToPushToFirebase.forEach(order => {
       this.cloudFirestoreService.addOrder(order).then(() => {
-        debugger;
         if (!this.isAlreadyInIndexedDBOrders(order, ordersToPushToFirebase)) {
           this.indexDbService.addToOrdersTable(order).then(() => {
             this.deleteOrderInIndexedDbOrdersOutbox(order.id);
@@ -175,7 +173,7 @@ export class CreateOrderComponent implements OnInit {
     this.toastr.success('Erfolgreich erstellt', 'Auftrag', successConfig);
   }
 
-  public toastMessageRecordAlreadyExists() {
+  public toastMessageOrderAlreadyExists() {
     const successConfig = {
       positionClass: 'toast-bottom-center',
       timeout: 2000
@@ -184,15 +182,15 @@ export class CreateOrderComponent implements OnInit {
   }
 
   public createOrder(formInput: any) {
-    this.newOrder = new Order(
+    const newOrder = new Order(
       formInput.companyName,
       formInput.location,
       new Date(),
       formInput.contactPerson
     );
-    this.newOrder.records = [];
+    newOrder.records = [];
 
-    this.createOrderIfOffline();
+    this.createOrderIfOffline(newOrder);
 
     // if (this.isConnected()) {
     //   this.createOrderIfOnline();
@@ -243,7 +241,7 @@ export class CreateOrderComponent implements OnInit {
                 console.error('can´t create order to firebase', e);
               });
           } else {
-            this.toastMessageRecordAlreadyExists();
+            this.toastMessageOrderAlreadyExists();
             return;
           }
         } else {
@@ -263,70 +261,89 @@ export class CreateOrderComponent implements OnInit {
     }
   }
 
-  public createOrderIfOffline() {
-    // check if new order is already in indexedDB orderOutbox
-    // if not, add to outbox otherwise ignore
-    // wenn mit server synchronisiert wird, muss indexedDB id durch firebase id ersetzt werden
-    this.indexDbService.getOrdersFromOutbox().then(ordersInOutbox => {
-      if (ordersInOutbox !== undefined) {
-        if (ordersInOutbox.length !== 0) {
-          const orders = [];
-          const newOrder = {
-            companyName: this.newOrder.companyName,
-            location: this.newOrder.location,
-            contactPerson: this.newOrder.contactPerson
-          };
-
-          ordersInOutbox.forEach(order => {
-            orders.push({
+  // check if order is in indexedDB ordersOutbox
+  public checkIfOrderIsInIndexedDBOrdersOutboxTable(order): Promise<boolean> {
+    let isAlreadyInOrdersOutboxTable = true;
+    return new Promise((resolve, reject) => {
+      this.indexDbService.getOrdersFromOutbox().then(ordersInOutbox => {
+        if (ordersInOutbox !== undefined) {
+          if (ordersInOutbox.length !== 0) {
+            const orders = [];
+            const newOrder = {
               companyName: order.companyName,
               location: order.location,
               contactPerson: order.contactPerson
+            };
+
+            ordersInOutbox.forEach(orderInOutbox => {
+              orders.push({
+                companyName: orderInOutbox.companyName,
+                location: orderInOutbox.location,
+                contactPerson: orderInOutbox.contactPerson
+              });
             });
-          });
-          this.isAlreadyInOutbox = _.findIndex(orders, o => _.isMatch(o, newOrder)) > -1;
-          if (!this.isAlreadyInOutbox) {
-            this.indexDbService.addOrderToOutbox(this.newOrder);
-            this.isAlreadyInOutbox = true;
+            isAlreadyInOrdersOutboxTable = _.findIndex(orders, o => _.isMatch(o, newOrder)) > -1;
+            resolve(isAlreadyInOrdersOutboxTable);
           } else {
-            this.toastMessageRecordAlreadyExists();
+            resolve(false);
           }
-        } else {
-          this.indexDbService.addOrderToOutbox(this.newOrder).then(() => {
-            this.toastMessageShowSuccess();
-          });
         }
-        this.isAlreadyInOutbox = true;
-      }
+      });
     });
   }
 
-  // Mittelteil wenn kontrolliert wurde ob bereits in firebase
+  // check if order is in indexedDB orders table
+  public checkIfOrderIsIndexedDBOrdersTable(order): Promise<boolean> {
+    let isAlreadyInOrdersTable = true;
+    return new Promise((resolve, reject) => {
+      this.indexDbService.getOrdersFromOrdersTable().then(_orders => {
+        if (_orders !== undefined) {
+          if (_orders.length !== 0) {
+            const orders = [];
+            const newOrder = {
+              companyName: order.companyName,
+              location: order.location,
+              contactPerson: order.contactPerson
+            };
 
-  // this.ordersObs.subscribe(_orders => {
-  //   this.orders = _orders;
-  //   if (this.orders.length !== 0) {
-  //     this.orders.forEach(order => {
-  //       this.indexDbService.getAllOrders().then(ordersInIndexedDB => {
-  //         ordersInIndexedDB.forEach(cachedOrder => {
-  //           this.orderIds.push(cachedOrder.id);
-  //         });
-  //         this.orders.forEach(_order => {
-  //           if (!this.orderIds.includes(_order.id)) {
-  //             this.cloudFirestoreService
-  //               .addOrder(_order)
-  //               .then(id => {
-  //                 this.toastMessageShowSuccess();
-  //                 this.router.navigate(['/order-details/' + id]);
-  //               })
-  //               .catch(e => {
-  //                 console.error('can´t create order to firebase', e);
-  //               });
-  //           }
-  //         });
-  //       });
-  //     });
-  //   }
+            _orders.forEach(orderInOutbox => {
+              orders.push({
+                companyName: orderInOutbox.companyName,
+                location: orderInOutbox.location,
+                contactPerson: orderInOutbox.contactPerson
+              });
+            });
+
+            isAlreadyInOrdersTable = _.findIndex(orders, o => _.isMatch(o, newOrder)) > -1;
+            resolve(isAlreadyInOrdersTable);
+          } else {
+            isAlreadyInOrdersTable = false;
+            resolve(isAlreadyInOrdersTable);
+          }
+        }
+      });
+    });
+  }
+
+  // check if new order is already in indexedDB orders or ordersOutbox table
+  // if not, add to ordersOutbox otherwise send toast message
+  public createOrderIfOffline(newOrder) {
+    this.checkIfOrderIsIndexedDBOrdersTable(newOrder).then(isInOrdersTable => {
+      if (!isInOrdersTable) {
+        return this.checkIfOrderIsInIndexedDBOrdersOutboxTable(newOrder).then(isInOrdersOutbox => {
+          if (!isInOrdersOutbox) {
+            this.indexDbService.addOrderToOutbox(newOrder).then(data => {
+              console.log('Added order to outbox', newOrder);
+            });
+          } else {
+            this.toastMessageOrderAlreadyExists();
+          }
+        });
+      } else {
+        return;
+      }
+    });
+  }
 
   public onSubmit() {
     this.createOrder(this.createOrderForm.value);
