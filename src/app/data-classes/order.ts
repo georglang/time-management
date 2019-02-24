8ba9bb8f43d8e2c2e4d12d10c983184d9713c85d
Georg Lang <mail@georglang.com>
2019-02-24 09:48:19 +0100

    added IFlattenOrder and removed unused properties from IOrder

--- src/app/data-classes/order.ts
+++ src/app/data-classes/order.ts
@@ -1,18 +1,22 @@
-import { ITimeRecord, TimeRecord } from './time-record';
+import { TimeRecord } from './time-record';
 
 export interface IOrder {
   companyName: string;
   contactPerson?: string;
-  id: string; // primery key, autoincremented by indexedDB
   location: string;
   records?: TimeRecord[];
   createdAt: Date;
 }
 
+export interface IFlattenOrder {
+  companyName: string;
+  contactPerson: string;
+  location: string;
+}
+
 export class Order implements IOrder {
   public companyName: string;
   public contactPerson: string;
-  public id: string;
   public location: string;
   public records: TimeRecord[];
   public createdAt: Date;
@@ -21,15 +25,11 @@ export class Order implements IOrder {
     companyName: string,
     location: string,
     createdAt: Date,
-    id?: string,
     contactPerson?: string,
     record?: TimeRecord
   ) {
     this.companyName = companyName;
     this.location = location;
-    if (id) {
-      this.id = id;
-    }
     if (record) {
       this.records.push(record);
     }
