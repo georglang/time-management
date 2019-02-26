407bc5a8a0de480a2d6ed8165fe1280508411070
Georg Lang <mail@georglang.com>
2019-02-26 21:37:32 +0100

    create record: if online and if offline

--- src/app/create-record/create-record.component.ts
+++ src/app/create-record/create-record.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
-import { IndexDBService } from '../service/index-db.service';
-import { TimeRecord } from '../data-classes/time-record';
+import { IndexedDBService } from '../service/indexedDb.service';
+import { ITimeRecord } from '../data-classes/time-record';
 import { ToastrService, Toast } from 'ngx-toastr';
-import { CloudFirestoreService } from './../service/cloud-firestore.service';
+import { CloudFirestoreService } from '../service/cloudFirestore.service';
 
 @Component({
   selector: 'app-create-record',
@@ -19,14 +19,13 @@ export class CreateRecordComponent implements OnInit {
     private router: Router,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    private indexDbService: IndexDBService,
+    private indexDbService: IndexedDBService,
     private toastr: ToastrService,
-    private firebaseService: CloudFirestoreService
+    private cloudFirestoreService: CloudFirestoreService
   ) {}
 
   ngOnInit() {
     this.createRecordForm = this.formBuilder.group({
-      id: [''],
       date: ['', Validators.required],
       description: ['', Validators.required],
       workingHours: ['', Validators.required]
@@ -34,8 +33,6 @@ export class CreateRecordComponent implements OnInit {
 
     this.route.params.subscribe(params => {
       this.paramId = params.id;
-      console.log('ParamId', this.paramId);
-
     });
   }
 
@@ -55,60 +52,102 @@ export class CreateRecordComponent implements OnInit {
     this.toastr.success('Erfolgreich erstellt', 'Eintrag', successConfig);
   }
 
-  public createUniqueId() {
-    const ID = () => {
-      const array = new Uint32Array(8);
-      window.crypto.getRandomValues(array);
-      let str = '';
-      for (let i = 0; i < array.length; i++) {
-        str += (i < 2 || i > 5 ? '' : '-') + array[i].toString(16).slice(-4);
-      }
-      return str;
+  public createRecordIfOnline(newRecord: ITimeRecord, orderId: string) {
+    newRecord.createdAt = new Date();
+    this.cloudFirestoreService
+      .checkIfRecordExistsInOrderInFirestore(orderId, newRecord)
+      .then(doesRecordExist => {
+        if (!doesRecordExist) {
+          this.cloudFirestoreService.addTimeRecord(orderId, newRecord).then(timeRecordResponse => {
+            console.log('Time Record Response', timeRecordResponse);
+            this.toastMessageRecordSuccessfulCreated();
+            this.router.navigate(['order-details', orderId]);
+          });
+        } else {
+          this.toastMessageRecordAlreadyExists();
+        }
+      });
+  }
+
+  public createRecordIfOffline(newRecord: ITimeRecord, orderId: string) {
+    this.indexDbService
+      .checkIfRecordIsInOrdersTable(newRecord, orderId)
+      .then(isAlreadyInOrdersTable => {
+        if (!isAlreadyInOrdersTable) {
+          this.indexDbService.addRecordToOrder(newRecord, orderId).then(data => {
+            console.log('Created in indexedDB', data);
+          });
+        } else {
+          this.toastMessageRecordAlreadyExists();
+        }
+      });
+  }
+
+  public toastMessageRecordAlreadyExists() {
+    const errorConfig = {
+      positionClass: 'toast-bottom-center',
+      timeout: 2000
     };
-    return ID();
+    this.toastr.error('Existiert bereits', 'Eintrag', errorConfig);
   }
 
-  public createRecord(record: TimeRecord, orderId: string) {
-    record.createdAt = new Date();
-    console.log('createRecord()', record, orderId);
-
-    if (this.isConnected()) {
-      this.firebaseService.addTimeRecord(orderId, record);
-    }
-
-
-
-
-    // if (!record.hasOwnProperty('id') || record.id === '') {
-
-    //   this.indexDbService.getAllRecords(orderId).then(records => {
-    //     console.log('Records', records);
-
-    //     if (records.length !== 0) {
-    //       const lastId = records[records.length - 1].id;
-    //       const idAsNumber = Number(lastId);
-    //       record.id = String(idAsNumber + 1);
-
-    //       console.log('Record Id: ', record.id);
-    //     } else {
-    //       record.id = '1';
-    //     }
-
-
-    //     this.indexDbService.addRecordToOrder(record, this.paramId)
-    //       .then((data) => {
-    //         console.log('DATA', data);
-
-    //         this.showSuccess();
-    //         this.navigateToOrderList();
-    //       });
-    //   });
-    // } else {
-    //   this.indexDbService.modifyOrder(this.paramId, record);
-    // }
+  public toastMessageRecordSuccessfulCreated() {
+    const successConfig = {
+      positionClass: 'toast-bottom-center',
+      timeout: 2000
+    };
+    this.toastr.success('Erfolgreich erstellt', 'Eintrag', successConfig);
   }
+
+  // if (this.isConnected()) {
+  //   record.createdAt = new Date();
+  //   this.indexDbService.addRecordToOrder(record, this.paramId);
+  //   this.cloudFirestoreService.addTimeRecord(orderId, record).then(timeRecordResponse => {
+  //     console.log('Time Record Response', timeRecordResponse);
+  //     this.router.navigate(['order-details', orderId]);
+  //   });
+  // } else {
+  //   console.log('No connection store in outbox');
+
+  //   this.indexDbService.addRecordToOrderOutbox(record, this.paramId).then(data => {
+  //     console.log('DATA', data);
+  //   });
+  //   this.indexDbService.addRecordToOrder(record, this.paramId).then(data => {});
+  //   this.showSuccess();
+  //   this.navigateToOrderList();
+
+  //   if (!record.hasOwnProperty('id') || record.id === '') {
+  //     this.indexDbService.getRecordsOfOrder(orderId).then(records => {
+  //       console.log('Records', records);
+
+  //       if (records.length !== 0) {
+  //         const lastId = records[records.length - 1].id;
+  //         const idAsNumber = Number(lastId);
+  //         record.id = String(idAsNumber + 1);
+
+  //         console.log('Record Id: ', record.id);
+  //       } else {
+  //         record.id = '1';
+  //       }
+
+  //       this.indexDbService.addRecordToOrder(record, this.paramId).then(data => {
+  //         console.log('DATA', data);
+
+  //         this.showSuccess();
+  //         this.navigateToOrderList();
+  //       });
+  //     });
+  //   } else {
+  //     // this.indexDbService.modifyOrder(this.paramId, record);
+  //   }
+  // }
 
   public onSubmit() {
-    this.createRecord(this.createRecordForm.value, this.paramId);
+    // if (this.isConnected()) {
+    //   this.createRecordIfOnline(this.createRecordForm.value, this.paramId);
+    // } else {
+    //   this.createRecordIfOffline(this.createRecordForm.value, this.paramId);
+    // }
+    this.createRecordIfOffline(this.createRecordForm.value, this.paramId);
   }
 }
