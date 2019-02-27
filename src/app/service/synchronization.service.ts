248c0f01f685a20c7dda7406fde66eee3214428b
Georg Lang <mail@georglang.com>
2019-02-27 21:25:10 +0100

    synchronize indexedDB recordsOutbox table with firebase

--- src/app/service/synchronization.service.ts
+++ src/app/service/synchronization.service.ts
@@ -12,37 +12,65 @@ export class SynchronizationService {
     private cloudFirestoreService: CloudFirestoreService
   ) {}
 
+  // read orders from indexedDB ordersOutbox table
   public synchronizeIndexedDBWithFirebase() {
     const ordersToPushToFirebase: string[] = [];
-    this.indexedDBService.getOrdersFromOutbox().then(ordersInOutbox => {
-      if (ordersInOutbox.length > 0) {
-        this.cloudFirestoreService.getDocumentsInOrdersCollection().then(ordersOnline => {
-          ordersInOutbox.forEach(orderInOutbox => {
-            const orderToCompare = {};
-            Object.assign(orderToCompare, orderInOutbox);
-            if (ordersOnline.length > 0) {
-              if (!this.compareIfOrderIsOnline(orderToCompare, ordersOnline)) {
+    this.indexedDBService.getOrdersFromOrdersOutbox().then(ordersInOutbox => {
+      if (ordersInOutbox !== undefined) {
+        if (ordersInOutbox.length > 0) {
+          this.cloudFirestoreService.getDocumentsInOrdersCollection().then(ordersOnline => {
+            ordersInOutbox.forEach(orderInOutbox => {
+              const orderToCompare = {};
+              Object.assign(orderToCompare, orderInOutbox);
+              if (ordersOnline.length > 0) {
+                if (!this.compareIfOrderIsOnline(orderToCompare, ordersOnline)) {
+                  ordersToPushToFirebase.push(orderInOutbox);
+                }
+              } else {
                 ordersToPushToFirebase.push(orderInOutbox);
               }
-            } else {
-              ordersToPushToFirebase.push(orderInOutbox);
-            }
+            });
+            this.sychronizeOrdersOutbox(ordersToPushToFirebase);
           });
-          this.sychronizeOrdersOutbox(ordersToPushToFirebase);
-        });
-      } else {
-        // if no orders are in ordersOutbox, synchronization is not necessary
-        return;
+        } else {
+          // if no orders are in ordersOutbox, synchronization is not necessary
+          return;
+        }
       }
     });
   }
+
+  // records from indexedDB recordsOutbox table
+  // update order with new records depending on orderId
+  public synchronizeIndexedDBRecordsTableWithFirebase() {
+    this.indexedDBService.getRecordsFromRecordsOutboxTable().then(records => {
+      if (records !== undefined) {
+        if (records.length > 0) {
+          records.forEach(record => {
+            this.cloudFirestoreService
+              .checkIfRecordExistsInOrderInFirestore(record.orderId, record)
+              .then(doesRecordExists => {
+                if (!doesRecordExists) {
+                  this.cloudFirestoreService.addTimeRecord(record.orderId, record).then(data => {
+                    this.indexedDBService.deleteRecordInOutbox(record.id);
+                  });
+                }
+              });
+          });
+        }
+      }
+    });
+  }
+
+  // ToDo: Refactoring: Gleiche Methode wie in edit-record.component update()
+  // sollte in service ausgelagert werden
+  public updateRecordByOrderId() {}
 
   // add order from indexedDB ordersOutbox table to firestore
   // after receiving the firestore id, add order to indexedDB orders table
   // delete order form indexedDB ordersOutbox
   public sychronizeOrdersOutbox(ordersToPushToFirebase) {
     ordersToPushToFirebase.forEach(order => {
-      debugger;
       this.cloudFirestoreService.addOrder(order).then(id => {
         if (!this.isAlreadyInIndexedDBOrders(order, ordersToPushToFirebase)) {
           const localId = order.id;
