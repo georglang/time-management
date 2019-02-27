1f70f769295eec53592e404d54ec85f3b1034d9c
Georg Lang <mail@georglang.com>
2019-02-27 21:39:27 +0100

    IndexedDb extended with recordsOutbox table

--- src/app/database/Database.ts
+++ src/app/database/Database.ts
@@ -1,17 +1,19 @@
 import Dexie from 'dexie';
 import relationships from 'dexie-relationships';
 import { ITimeRecord } from '../data-classes/time-record';
-import { IOrder } from '../data-classes/order';
+import { IOrder } from '../data-classes/Order';
 
 export class Database extends Dexie {
   public orders: Dexie.Table<IOrder, 'string'>;
-  public outboxForOrders: Dexie.Table<IOrder, 'string'>;
+  public ordersOutbox: Dexie.Table<IOrder, 'string'>;
+  public recordsOutbox: Dexie.Table<ITimeRecord, 'string'>;
 
   constructor() {
     super('TimeRecords', { addons: [relationships] });
     this.version(1).stores({
       orders: '++id, companyName, location, contactPerson, records',
-      outboxForOrders: '++id, companyName, location, contactPerson, records'
+      ordersOutbox: '++id, companyName, location, contactPerson, records',
+      recordsOutbox: '++id, orderId, date, description, workingHours'
     });
   }
 }
