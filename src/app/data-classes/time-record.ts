042e73b7f41d92ca727281156833adcfd7beca3d
Georg Lang <mail@georglang.com>
2019-02-27 21:40:59 +0100

    extended ITimeRecord with orderId

--- src/app/data-classes/time-record.ts
+++ src/app/data-classes/time-record.ts
@@ -1,23 +1,26 @@
 export interface ITimeRecord {
-  id?: string;
   date: string;
   description: string;
   workingHours: number;
-  createdAt: Date;
+  createdAt?: Date;
+  id?: string;
+  orderId?: string;
 }
 
 export class TimeRecord implements ITimeRecord {
-  public id?: string;
   public date: string;
   public description: string;
   public workingHours: number;
   public location: string;
   public createdAt: Date;
+  public id: string;
+  public orderId: string;
 
-  constructor(date: string, description: string, workingHours: number, createdAt: Date, id?: string) {
+  constructor(date: string, description: string, workingHours: number, createdAt: Date, id: string, orderId: string) {
     this.date = date;
     this.description = description;
     this.workingHours = workingHours;
     this.id = id;
+    this.orderId = orderId;
   }
 }
