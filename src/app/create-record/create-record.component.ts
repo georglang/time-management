bc93e1f2c7aa23119adcaad6be25ffb41d898766
Georg Lang <mail@georglang.com>
2019-03-01 15:52:41 +0100

    create record: removed unused code

--- src/app/create-record/create-record.component.ts
+++ src/app/create-record/create-record.component.ts
@@ -102,51 +102,6 @@ export class CreateRecordComponent implements OnInit {
       });
   }
 
-
-
-  // if (this.isConnected()) {
-  //   record.createdAt = new Date();
-  //   this.indexDbService.addRecordToOrder(record, this.paramId);
-  //   this.cloudFirestoreService.addTimeRecord(orderId, record).then(timeRecordResponse => {
-  //     console.log('Time Record Response', timeRecordResponse);
-  //     this.router.navigate(['order-details', orderId]);
-  //   });
-  // } else {
-  //   console.log('No connection store in outbox');
-
-  //   this.indexDbService.addRecordToOrderOutbox(record, this.paramId).then(data => {
-  //     console.log('DATA', data);
-  //   });
-  //   this.indexDbService.addRecordToOrder(record, this.paramId).then(data => {});
-  //   this.showSuccess();
-  //   this.navigateToOrderList();
-
-  //   if (!record.hasOwnProperty('id') || record.id === '') {
-  //     this.indexDbService.getRecordsOfOrder(orderId).then(records => {
-  //       console.log('Records', records);
-
-  //       if (records.length !== 0) {
-  //         const lastId = records[records.length - 1].id;
-  //         const idAsNumber = Number(lastId);
-  //         record.id = String(idAsNumber + 1);
-
-  //         console.log('Record Id: ', record.id);
-  //       } else {
-  //         record.id = '1';
-  //       }
-
-  //       this.indexDbService.addRecordToOrder(record, this.paramId).then(data => {
-  //         console.log('DATA', data);
-
-  //         this.showSuccess();
-  //         this.navigateToOrderList();
-  //       });
-  //     });
-  //   } else {
-  //     // this.indexDbService.modifyOrder(this.paramId, record);
-  //   }
-  // }
-
   public onSubmit() {
     // if (this.isConnected()) {
     //   this.createRecordIfOnline(this.createRecordForm.value, this.paramId);
