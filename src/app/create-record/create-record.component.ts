4f85ffb4e88bd1091cd68cf8db6fff97c2746555
Georg Lang <mail@georglang.com>
2019-03-01 15:49:52 +0100

    create record if offline, modified

--- src/app/create-record/create-record.component.ts
+++ src/app/create-record/create-record.component.ts
@@ -2,9 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 import { IndexedDBService } from '../service/indexedDb.service';
-import { ITimeRecord } from '../data-classes/time-record';
+import { ITimeRecord } from '../data-classes/ITimeRecords';
 import { ToastrService, Toast } from 'ngx-toastr';
 import { CloudFirestoreService } from '../service/cloudFirestore.service';
+import { MessageService } from './../service/message.service';
 
 @Component({
   selector: 'app-create-record',
@@ -13,7 +14,7 @@ import { CloudFirestoreService } from '../service/cloudFirestore.service';
 })
 export class CreateRecordComponent implements OnInit {
   public createRecordForm: FormGroup;
-  private paramId;
+  private routeParamOrderId;
 
   constructor(
     private router: Router,
@@ -21,7 +22,8 @@ export class CreateRecordComponent implements OnInit {
     private formBuilder: FormBuilder,
     private indexDbService: IndexedDBService,
     private toastr: ToastrService,
-    private cloudFirestoreService: CloudFirestoreService
+    private cloudFirestoreService: CloudFirestoreService,
+    private messageService: MessageService
   ) {}
 
   ngOnInit() {
@@ -32,16 +34,19 @@ export class CreateRecordComponent implements OnInit {
     });
 
     this.route.params.subscribe(params => {
-      this.paramId = params.id;
+      this.routeParamOrderId = params.id;
     });
   }
+
+  // ToDo: recordId in firebase: Lokale id mit Online ID ersetzen
+  // ToDo: createRecordIfOffline testen
 
   public isConnected() {
     return navigator.onLine;
   }
 
   public navigateToOrderList() {
-    this.router.navigate(['/order-details', this.paramId]);
+    this.router.navigate(['/order-details', this.routeParamOrderId]);
   }
 
   public showSuccess() {
@@ -60,44 +65,44 @@ export class CreateRecordComponent implements OnInit {
         if (!doesRecordExist) {
           this.cloudFirestoreService.addTimeRecord(orderId, newRecord).then(timeRecordResponse => {
             console.log('Time Record Response', timeRecordResponse);
-            this.toastMessageRecordSuccessfulCreated();
+            this.messageService.recordSuccessfulCreated();
             this.router.navigate(['order-details', orderId]);
           });
         } else {
-          this.toastMessageRecordAlreadyExists();
+          this.messageService.recordAlreadyExists();
         }
       });
   }
 
+  // if the order which the record belongs to is in ordersOutbox table, add record to order in ordersOutbox table
+  // if the order which the record belongs to is not in recordsOutbox table, add record to recordsOutbox table
   public createRecordIfOffline(newRecord: ITimeRecord, orderId: string) {
     this.indexDbService
-      .checkIfRecordIsInOrdersTable(newRecord, orderId)
-      .then(isAlreadyInOrdersTable => {
-        if (!isAlreadyInOrdersTable) {
-          this.indexDbService.addRecordToOrder(newRecord, orderId).then(data => {
-            console.log('Created in indexedDB', data);
+      .checkByIdIfOrderIsInIndexedDBOrdersOutboxTable(orderId)
+      .then(isOrderInOrdersOutbox => {
+        if (isOrderInOrdersOutbox) {
+          this.indexDbService.addRecordToOrdersOutboxTable(newRecord, orderId).then(data => {
+            this.messageService.recordSuccessfulCreated();
           });
         } else {
-          this.toastMessageRecordAlreadyExists();
+          this.indexDbService
+            .checkIfRecordIsInRecordsOutboxTable(newRecord, orderId)
+            .then(isAlreadyInRecordsOutboxTable => {
+              if (!isAlreadyInRecordsOutboxTable) {
+                newRecord.orderId = orderId;
+                this.indexDbService.addRecordToRecordsOutboxTable(newRecord)
+                  .then(() => {
+                    this.messageService.recordSuccessfulCreated();
+                  });
+              } else {
+                this.messageService.recordAlreadyExists();
+              }
+            });
         }
       });
   }
 
-  public toastMessageRecordAlreadyExists() {
-    const errorConfig = {
-      positionClass: 'toast-bottom-center',
-      timeout: 2000
-    };
-    this.toastr.error('Existiert bereits', 'Eintrag', errorConfig);
-  }
 
-  public toastMessageRecordSuccessfulCreated() {
-    const successConfig = {
-      positionClass: 'toast-bottom-center',
-      timeout: 2000
-    };
-    this.toastr.success('Erfolgreich erstellt', 'Eintrag', successConfig);
-  }
 
   // if (this.isConnected()) {
   //   record.createdAt = new Date();
@@ -148,6 +153,6 @@ export class CreateRecordComponent implements OnInit {
     // } else {
     //   this.createRecordIfOffline(this.createRecordForm.value, this.paramId);
     // }
-    this.createRecordIfOffline(this.createRecordForm.value, this.paramId);
+    this.createRecordIfOffline(this.createRecordForm.value, this.routeParamOrderId);
   }
 }
