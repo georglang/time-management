ac9cff42ad478f14c457a713ad3a9ce660b5b2a9
Georg Lang <mail@georglang.com>
2019-03-11 18:57:50 +0100

    Renaming from ITimeRecords to TimeRecord

--- src/app/data-classes/TimeRecords.ts
+++ src/app/data-classes/TimeRecords.ts
@@ -1,8 +1,9 @@
 // ToDo: Evt. createdAt entfernen, da nicht benoetigt
-
+import { Timestamp } from '@firebase/firestore-types';
+'@firebase/firestore-types'
 
 export interface ITimeRecord {
-  date: string;
+  date: any;
   description: string;
   workingHours: number;
   id?: string;      // is necessary when deleting local record after synchronization
@@ -10,14 +11,14 @@ export interface ITimeRecord {
 }
 
 export class TimeRecord implements ITimeRecord {
-  public date: string;
+  public date: any;
   public description: string;
   public workingHours: number;
   public location: string;
   public id: string;
   public orderId: string;
 
-  constructor(date: string, description: string, workingHours: number, id: string, orderId: string) {
+  constructor(date: any, description: string, workingHours: number, id: string, orderId: string) {
     this.date = date;
     this.description = description;
     this.workingHours = workingHours;
