d2cb748abdc99074016aac2414b8d5a89e45a5ab
Georg Lang <mail@georglang.com>
2019-09-23 19:14:54 +0200

    app.component.spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'angular-pwa'`, async(() => {
+  it(`should have as title 'time-management'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-pwa');
+    expect(app.title).toEqual('time-management');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-pwa!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to time-management!');
   }));
 });
