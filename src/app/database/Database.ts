0d50f8c77d8439910a5faa942f7761cd16639b44
Georg Lang <mail@georglang.com>
2019-09-23 19:15:35 +0200

    renaming TimeRecords to TimeManagement

--- src/app/database/Database.ts
+++ src/app/database/Database.ts
@@ -1,6 +1,6 @@
 import Dexie from 'dexie';
 import relationships from 'dexie-relationships';
-import { ITimeRecord } from '../data-classes/ITimeRecords';
+import { ITimeRecord } from '../data-classes/TimeRecords';
 import { IOrder } from '../data-classes/Order';
 
 export class Database extends Dexie {
@@ -9,7 +9,7 @@ export class Database extends Dexie {
   public recordsOutbox: Dexie.Table<ITimeRecord, 'string'>;
 
   constructor() {
-    super('TimeRecords', { addons: [relationships] });
+    super('TimeManagement', { addons: [relationships] });
     this.version(1).stores({
       orders: '++id, companyName, location, contactPerson, records',
       ordersOutbox: '++id, companyName, location, contactPerson, records',
