9443dcaad83ed575e83b34109cd337fbc93a6549
Georg Lang <mail@georglang.com>
2019-10-10 18:10:04 +0200

    TimeRecords: id and orderId as optional properties

--- src/app/data-classes/TimeRecords.ts
+++ src/app/data-classes/TimeRecords.ts
@@ -1,12 +1,12 @@
 // ToDo: Evt. createdAt entfernen, da nicht benoetigt
 import { Timestamp } from '@firebase/firestore-types';
-'@firebase/firestore-types'
+'@firebase/firestore-types';
 
 export interface ITimeRecord {
   date: any;
   description: string;
   workingHours: number;
-  id?: string;      // is necessary when deleting local record after synchronization
+  id?: string; // is necessary when deleting local record after synchronization
   orderId?: string;
 }
 
@@ -16,9 +16,9 @@ export class TimeRecord implements ITimeRecord {
   public workingHours: number;
   public location: string;
   public id: string;
-  public orderId: string;
+  public orderId: any;
 
-  constructor(date: any, description: string, workingHours: number, id: string, orderId: string) {
+  constructor(date: any, description: string, workingHours: number, id?: string, orderId?: string) {
     this.date = date;
     this.description = description;
     this.workingHours = workingHours;
