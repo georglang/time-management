1d04e043009d65ee6a9322302119e274b559c1ab
Georg Lang <mail@georglang.com>
2020-10-29 20:24:18 +0100

    Time Records: properties employee and excluded added

--- src/app/data-classes/TimeRecords.ts
+++ src/app/data-classes/TimeRecords.ts
@@ -1,27 +1,38 @@
 // ToDo: Evt. createdAt entfernen, da nicht benoetigt
-import { Timestamp } from '@firebase/firestore-types';
-'@firebase/firestore-types';
+import { Timestamp } from "@firebase/firestore-types";
+("@firebase/firestore-types");
 
 export interface ITimeRecord {
   date: any;
   description: string;
   workingHours: number;
+  employee: string;
   id?: string; // is necessary when deleting local record after synchronization
   orderId?: string;
+  excluded?: boolean;
 }
 
 export class TimeRecord implements ITimeRecord {
   public date: any;
   public description: string;
   public workingHours: number;
+  public employee: string;
   public location: string;
   public id: string;
   public orderId: any;
 
-  constructor(date: any, description: string, workingHours: number, id?: string, orderId?: string) {
+  constructor(
+    date: any,
+    description: string,
+    workingHours: number,
+    employee: string,
+    id?: string,
+    orderId?: string
+  ) {
     this.date = date;
     this.description = description;
     this.workingHours = workingHours;
+    this.employee = employee;
     this.id = id;
     this.orderId = orderId;
   }
