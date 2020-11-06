0c4b062b61419e66df63c1d3243877d1b833f38d
Georg Lang <mail@georglang.com>
2020-11-06 10:07:33 +0100

    order-details: indexedDB removed

--- src/app/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail.component.ts
@@ -4,14 +4,12 @@ import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
 import { MatTableDataSource } from "@angular/material/table";
 import { SelectionModel } from "@angular/cdk/collections";
 
-// import { IndexedDBService } from '../service/indexedDb-service/indexedDb.service';
 import { DateAdapter } from "@angular/material/core";
 import { TimeRecord, ITimeRecord } from "../data-classes/TimeRecords";
 import { ConfirmDeleteDialogComponent } from "./../confirm-delete-dialog/confirm-delete-dialog.component";
 import { ToastrService } from "ngx-toastr";
 import { FirestoreOrderService } from "../service/firestore-order-service/firestore-order.service";
 import { FirestoreRecordService } from "../service/firestore-record-service/firestore-record.service";
-// import { SynchronizeIdxDBWithFirebaseService } from "../service/synchronize-idxDb-with-firebase-service/synchronize-idxDb-with-firebase.service";
 import { MessageService } from "../service/message-service/message.service";
 
 import * as moment from "moment";
@@ -61,7 +59,6 @@ export class OrderDetailComponent implements OnInit {
   public selectedRecord: ITimeRecord;
   public showButtonsIfRecordIsSelected: boolean = false;
 
-  private orderIds: number[] = [];
   public pdf = new jsPDF() as jsPDFWithPlugin;
 
   public selected = false;
@@ -70,77 +67,20 @@ export class OrderDetailComponent implements OnInit {
     private route: ActivatedRoute,
     private router: Router,
     private dateAdapter: DateAdapter<Date>,
-    // private indexedDbService: IndexedDBService,
     private toastrService: ToastrService,
     public dialog: MatDialog,
-    private readonly connectionService: ConnectionService,
     private firestoreOrderService: FirestoreOrderService,
-    private firestoreRecordService: FirestoreRecordService,
-    // private synchronizeIdxDBWithFirebase: SynchronizeIdxDBWithFirebaseService,
-    private messageService: MessageService
+    private firestoreRecordService: FirestoreRecordService
   ) {
     this.dateAdapter.setLocale("de");
     this.columns = ["Date", "Description", "Time", "Delete"];
     this.sumOfWorkingHours = 0;
-
-    if (window.indexedDB) {
-      console.log("IndexedDB is supported");
-    }
   }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
       this.paramOrderId = params["id"];
-      const order = this.getOrderByIdFromCloudDatabase(this.paramOrderId);
-
-      // if (this.isOnline()) {
-      //   this.getOrderByIdFromCloudDatabase(this.paramOrderId);
-      // }
-
-      // if (this.isOnline()) {
-      //   this.getRecords(this.paramId);
-      // } else {
-      //   console.log('Get records from indexedDB');
-      // }
-
-      // this.getOrderById(this.paramId);
-      // this.getOrderByIdFromIndexedDbOrders(this.paramOrderId);
-
-      // this.getRecordsFromRecordsOutbox(this.paramId).then(records => {
-      //   if (records !== undefined) {
-      //     this.records = records;
-      //     this.dataSource = new MatTableDataSource<ITimeRecord>(this.records);
-      //   }
-      // });
-
-      // this.records = this.getRecordsFromRecordsOutbox(this.paramId);
-    });
-
-    this.connectionService.monitor().subscribe((isOnline) => {
-      if (this._isOnline) {
-        // this.indexedDbService.getOrdersFromOrdersOutbox().then(orders => {
-        //   if (orders.length !== 0) {
-        //     orders.forEach(order => {
-        //       const id = order.id;
-        //       delete order.id;
-        //       this.firestoreOrderService.addOrder(order).then(() => {
-        //         this.indexedDbService.getOrdersFromOrdersTable().then(ordersInIndexedDB => {
-        //           ordersInIndexedDB.forEach(cachedOrder => {
-        //             this.orderIds.push(cachedOrder.id);
-        //           });
-        //           orders.forEach(_order => {
-        //             if (!this.orderIds.includes(_order.id)) {
-        //               this.indexedDbService.addToOrdersTable(order).then(() => {
-        //                 this.indexedDbService.deleteOrderInOrdersOutbox(id);
-        //               });
-        //             }
-        //           });
-        //         });
-        //       });
-        //     });
-        //   }
-        // });
-      }
+      this.getOrderByIdFromCloudDatabase(this.paramOrderId);
     });
   }
 
@@ -151,10 +91,6 @@ export class OrderDetailComponent implements OnInit {
   public navigateToOrderList(): void {
     this.router.navigate(["/"]);
   }
-
-  //
-  // Online Handling
-  //
 
   public getOrderByIdFromCloudDatabase(orderId: string) {
     this.firestoreOrderService
@@ -199,87 +135,10 @@ export class OrderDetailComponent implements OnInit {
     }
   }
 
-  //
-  // Offline
-  //
-
-  // public getOrderByIdFromIndexedDbOrders(orderId: any) {
-  //   if (typeof orderId !== 'string') {
-  //     this.indexedDbService.getOrderById(orderId).then((order: IOrder[]) => {
-  //       if (order.length > 0) {
-  //         this.order = order[0];
-  //         this.setRecordDataSource(this.order.records);
-  //       }
-  //     });
-  //   } else {
-  //     this.indexedDbService.getOrderByFirebaseId(orderId).then((order: IOrder[]) => {
-  //       if (order.length > 0) {
-  //         this.order = order[0];
-  //         this.setRecordDataSource(this.order.records);
-  //       }
-  //     });
-  //   }
-  // }
-
-  // records from firebase and indexedDB
   public getRecords(orderId: string) {
     const records: TimeRecord[] = [];
-
-    if (this.isOnline()) {
-      // this.firestoreRecordService.getRecords();
-
-      // this.firestoreRecordService.x(orderId).subscribe(recordsInFirebase => {
-      //   if (recordsInFirebase !== undefined) {
-      //     if (recordsInFirebase.length > 0) {
-      //       recordsInFirebase.forEach(record => {
-      //         record.date = moment.unix(record.date.seconds).format('MM.DD.YYYY');
-      //         records.push(record);
-      //         this.getSumOfWorkingHours(records);
-      //       });
-      //     } else {
-      //       this.sumOfWorkingHours = 0;
-      //     }
-      //   }
-      // });
-      this.dataSource.data = records;
-    } else {
-      console.log("Get Data from Indexed DB");
-    }
+    this.dataSource.data = records;
   }
-
-  public getRecordsFromOrdersOutbox(orderId: string) {
-    // this.indexedDbService.getOrdersFromOrdersOutbox().then(records => {
-    // records.forEach(record => {
-    //   record.date = new Date(record.date);
-    // });
-    // console.log('Records from Outbox', records);
-    // this.dataSource = new MatTableDataSource<ITimeRecord>(records);
-    // });
-  }
-
-  // public getRecordsFromRecordsOutbox(orderId: string): Promise<any> {
-  // return this.indexedDbService.getRecordsFromRecordsOutboxTable().then(records => {
-  //   return records;
-  // });
-  // }
-
-  // public getOrderById(orderId: string) {
-  //   if (this.isOnline()) {
-  //     this.firestoreOrderService.getOrderById(orderId).then(order => {
-  //       this.order = order;
-  //     });
-  //   } else {
-  //     this.indexDbService.getOrderById(orderId).then(order => {
-  //       if (order.length !== 0) {
-  //         if (order[0].hasOwnProperty('records')) {
-  //           this.order = order[0];
-  //           this.records = order[0].records;
-  //           this.dataSource = new MatTableDataSource<ITimeRecord>(this.records);
-  //         }
-  //       }
-  //     });
-  //   }
-  // }
 
   public createNewRecord() {
     this.router.navigate([
@@ -342,17 +201,8 @@ export class OrderDetailComponent implements OnInit {
       .deleteRecord(this.paramOrderId, recordId)
       .then((data) => {
         this.showDeleteMessage();
-        // load records after deletion
         this.getRecordsFromCloudDatabase(this.paramOrderId);
-
-        // ToDo Offline Handling
-        // delete record in indexedDB orders table
-        // this.indexDbService.deleteRecordInOrdersTable(this.paramId, _recordId).then(() => {
-        //   this.messageService.recordDeletedSuccessful();
-        // });
       });
-
-    // this.indexedDbService.deleteRecordInOrdersTable(this.paramOrderId, recordId).then(data => {});
   }
 
   /** Whether the number of selected elements matches the total number of rows. */
@@ -389,17 +239,6 @@ export class OrderDetailComponent implements OnInit {
 
     this.selection.selected.forEach((selectedRecord) => {
       this.dateFormated = moment(selectedRecord["date"]).format("DD.MM.YYYY");
-
-      // recordsToPrint.push(
-      //   new TimeRecord(
-      //     this.dateFormated,
-      //     selectedRecord["description"],
-      //     selectedRecord["workingHours"],
-      //     selectedRecord["employee"],
-      //     "",
-      //     ""
-      //   )
-      // );
 
       recordsToPrint.push([
         this.dateFormated,
@@ -478,13 +317,7 @@ export class OrderDetailComponent implements OnInit {
     });
   }
 
-  // Beim allgemeinen synchronisieren muss zuerst herausgefunden werden, was ueberhaut sychronisiert werden muss
-  // Erstellen Order und Record Offline: schauen in ordersOutbox
-  public synchronizeOrdersAndRecords() {
-    // this.synchronizeIdxDBWithFirebase.synchronizeWithFirebase();
-  }
-
-  showEditAndDeleteButton(selectedRecord: ITimeRecord) {
+  public showEditAndDeleteButton(selectedRecord: ITimeRecord) {
     this.selectedRecord = selectedRecord;
     if (this.highlighted.selected.length == 0) {
       this.showButtonsIfRecordIsSelected = false;
