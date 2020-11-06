5add5cb806d8af8652cf304947bf767c3c8612ae
Georg Lang <mail@georglang.com>
2020-11-06 12:28:06 +0100

    delete-dialoge: passwort protection

--- src/app/confirm-delete-dialog/confirm-delete-dialog.component.ts
+++ src/app/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -1,13 +1,32 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-confirm-delete-dialog',
-  templateUrl: './confirm-delete-dialog.component.html',
-  styleUrls: ['./confirm-delete-dialog.component.sass']
+  selector: "app-confirm-delete-dialog",
+  templateUrl: "./confirm-delete-dialog.component.html",
+  styleUrls: ["./confirm-delete-dialog.component.sass"],
 })
 export class ConfirmDeleteDialogComponent implements OnInit {
   public modalTitle: string;
+  public passwordCorrect: string;
+  public hide = true;
+  public isPasswortCorrect = false;
+
+  public signin: FormGroup = new FormGroup({
+    password: new FormControl("", [Validators.required, Validators.min(3)]),
+  });
+
+  get passwordInput() {
+    return this.signin.get("password");
+  }
+
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.signin.controls["password"].valueChanges.subscribe((input) => {
+      if (input === "Löwenbräu") {
+        this.isPasswortCorrect = true;
+      }
+    });
+  }
 }
