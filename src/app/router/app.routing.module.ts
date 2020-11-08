ad3d32514db0481f5e7d72b3ebea3e20a13c1b91
Georg Lang <mail@georglang.com>
2020-11-08 07:06:25 +0100

    app.routing: edit route

--- src/app/router/app.routing.module.ts
+++ src/app/router/app.routing.module.ts
@@ -1,30 +1,32 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes, ActivatedRoute, ParamMap } from '@angular/router';
-import { OrderListComponent } from '../order-list/order-list.component';
-import { CreateOrderComponent } from '../create-order/create-order.component';
-import { OrderDetailComponent } from '../order-detail/order-detail.component';
-import { EditRecordComponent } from '../edit-record/edit-record.component';
-import { CreateRecordComponent } from '../create-record/create-record.component';
+import { NgModule, Component } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { OrderListComponent } from "../order-list/order-list.component";
+import { CreateOrderComponent } from "../create-order/create-order.component";
+import { OrderDetailComponent } from "../order-detail/order-detail.component";
+import { EditRecordComponent } from "../edit-record/edit-record.component";
+import { CreateRecordComponent } from "../create-record/create-record.component";
+import { EditOrderComponent } from "../edit-order/edit-order.component";
 
 const appRoutes = [
-  { path: '', component: OrderListComponent },
-  { path: 'create-order', component: CreateOrderComponent },
+  { path: "", component: OrderListComponent },
+  { path: "create-order", component: CreateOrderComponent },
   {
-    path: 'order-details/:id',
+    path: "order-details/:id",
     children: [
-      { path: '', component: OrderDetailComponent },
-      { path: 'create-record', component: CreateRecordComponent },
-      { path: 'edit-record/:id', component: EditRecordComponent }
-    ]
+      { path: "", component: OrderDetailComponent },
+      { path: "create-record", component: CreateRecordComponent },
+      { path: "edit-record/:id", component: EditRecordComponent },
+    ],
   },
+  { path: "edit-order/:id", component: EditOrderComponent },
   // * wildcard if the requested URL doesn´t match any path in the URL
   // could also be a 404 page
-  { path: '**', component: OrderListComponent, pathMatch: 'full' }
+  { path: "**", component: OrderListComponent, pathMatch: "full" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule],
-  providers: []
+  providers: [],
 })
 export class AppRoutingModule {}
