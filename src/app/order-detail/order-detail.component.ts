6d42149783de3f6c3862f8e70820b1f9565f26dc
Georg Lang <mail@georglang.com>
2020-11-09 14:48:40 +0100

    order-details: show additional buttons if logged-in

--- src/app/order-detail/order-detail.component.ts
+++ src/app/order-detail/order-detail.component.ts
@@ -1,37 +1,37 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Router, ActivatedRoute } from "@angular/router";
-import { MatDialog, MatDialogConfig } from "@angular/material/dialog";
-import { MatTableDataSource } from "@angular/material/table";
-import { SelectionModel } from "@angular/cdk/collections";
+import { Component, OnInit, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
 
-import { DateAdapter } from "@angular/material/core";
-import { TimeRecord, ITimeRecord } from "../data-classes/TimeRecords";
-import { ConfirmDeleteDialogComponent } from "./../confirm-delete-dialog/confirm-delete-dialog.component";
-import { ToastrService } from "ngx-toastr";
-import { FirestoreOrderService } from "../service/firestore-order-service/firestore-order.service";
-import { FirestoreRecordService } from "../service/firestore-record-service/firestore-record.service";
+import { DateAdapter } from '@angular/material/core';
+import { TimeRecord, ITimeRecord } from '../data-classes/TimeRecords';
+import { ConfirmDeleteDialogComponent } from './../confirm-delete-dialog/confirm-delete-dialog.component';
+import { ToastrService } from 'ngx-toastr';
+import { FirestoreOrderService } from '../service/firestore-order-service/firestore-order.service';
+import { FirestoreRecordService } from '../service/firestore-record-service/firestore-record.service';
 
-import * as moment from "moment";
-import { ConnectionService } from "ng-connection-service";
-import { IOrder } from "../data-classes/Order";
-moment.locale("de");
+import * as moment from 'moment';
+import { ConnectionService } from 'ng-connection-service';
+import { IOrder } from '../data-classes/Order';
+moment.locale('de');
 
 // import * as jsPDF from 'jspdf';
 // import "jspdf-autotable";
 
-import { jsPDF } from "jspdf";
-import "jspdf-autotable";
-import { UserOptions } from "jspdf-autotable";
-import { ConfirmPrintDialogComponent } from "../confirm-print-dialog/confirm-print-dialog.component";
+import { jsPDF } from 'jspdf';
+import 'jspdf-autotable';
+import { UserOptions } from 'jspdf-autotable';
+import { ConfirmPrintDialogComponent } from '../confirm-print-dialog/confirm-print-dialog.component';
 
 interface jsPDFWithPlugin extends jsPDF {
   autoTable: (options: UserOptions) => jsPDF;
 }
 
 @Component({
-  selector: "app-order-detail",
-  templateUrl: "./order-detail.component.html",
-  styleUrls: ["./order-detail.component.sass"],
+  selector: 'app-order-detail',
+  templateUrl: './order-detail.component.html',
+  styleUrls: ['./order-detail.component.sass'],
 })
 export class OrderDetailComponent implements OnInit {
   @Input() customer;
@@ -43,12 +43,12 @@ export class OrderDetailComponent implements OnInit {
   public totalTime = 0.0;
   public records: ITimeRecord[] = [];
   public displayedColumns = [
-    "select",
-    "date",
-    "description",
-    "workingHours",
-    "employee",
-    "excluded",
+    'select',
+    'date',
+    'description',
+    'workingHours',
+    'employee',
+    'excluded',
   ];
   public dataSource: MatTableDataSource<ITimeRecord>;
   public hasRecordsFound: boolean = false;
@@ -58,6 +58,8 @@ export class OrderDetailComponent implements OnInit {
   public highlighted = new SelectionModel<ITimeRecord>(false, []);
   public selectedRecord: ITimeRecord;
   public showButtonsIfRecordIsSelected: boolean = false;
+  public showPrintButton: boolean = false;
+  public showDeleteButton: boolean = false;
 
   public pdf = new jsPDF() as jsPDFWithPlugin;
 
@@ -72,14 +74,14 @@ export class OrderDetailComponent implements OnInit {
     private firestoreOrderService: FirestoreOrderService,
     private firestoreRecordService: FirestoreRecordService
   ) {
-    this.dateAdapter.setLocale("de");
-    this.columns = ["Date", "Description", "Time", "Delete"];
+    this.dateAdapter.setLocale('de');
+    this.columns = ['Date', 'Description', 'Time', 'Delete'];
     this.sumOfWorkingHours = 0;
   }
 
   ngOnInit() {
     this.route.params.subscribe((params) => {
-      this.paramOrderId = params["id"];
+      this.paramOrderId = params['id'];
       this.getOrderByIdFromCloudDatabase(this.paramOrderId);
     });
   }
@@ -89,7 +91,7 @@ export class OrderDetailComponent implements OnInit {
   }
 
   public navigateToOrderList(): void {
-    this.router.navigate(["/"]);
+    this.router.navigate(['/']);
   }
 
   public getOrderByIdFromCloudDatabase(orderId: string) {
@@ -140,13 +142,13 @@ export class OrderDetailComponent implements OnInit {
 
   public createNewRecord() {
     this.router.navigate([
-      "/order-details/" + this.paramOrderId + /create-record/,
+      '/order-details/' + this.paramOrderId + /create-record/,
     ]);
   }
 
   public editRecord(record: ITimeRecord) {
     this.router.navigate([
-      "/order-details/" + this.paramOrderId + /edit-record/ + record.id,
+      '/order-details/' + this.paramOrderId + /edit-record/ + record.id,
     ]);
   }
 
@@ -160,20 +162,20 @@ export class OrderDetailComponent implements OnInit {
 
   public showDeleteMessage() {
     const successConfig = {
-      positionClass: "toast-bottom-center",
+      positionClass: 'toast-bottom-center',
       timeout: 500,
     };
-    this.toastrService.error("Erfolgreich gelöscht", "Eintrag", successConfig);
+    this.toastrService.error('Erfolgreich gelöscht', 'Eintrag', successConfig);
   }
 
   public showSuccessMessage() {
     const successConfig = {
-      positionClass: "toast-bottom-center",
+      positionClass: 'toast-bottom-center',
       timeout: 500,
     };
     this.toastrService.success(
-      "Erfolgreich erstellt",
-      "Eintrag",
+      'Erfolgreich erstellt',
+      'Eintrag',
       successConfig
     );
   }
@@ -189,7 +191,8 @@ export class OrderDetailComponent implements OnInit {
     );
     dialogRef.afterClosed().subscribe((shouldPrint) => {
       if (shouldPrint) {
-        this.print();
+        this.showPrintButton = true;
+        this.showDeleteButton = true;
       }
     });
   }
@@ -226,39 +229,43 @@ export class OrderDetailComponent implements OnInit {
     return numSelected === numRows;
   }
 
-  /** Selects all rows if they are not all selected; otherwise clear selection. */ masterToggle() {
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  public masterToggle() {
     this.isAllSelected()
       ? this.selection.clear()
-      : // this.dataSource.data.forEach(row => this.selection.select(row));
+      : this.dataSource.data.forEach((row) => this.selection.select(row));
+  }
 
-        this.dataSource.data.forEach((row) => {
-          if (!row.excluded) {
-            this.selection.select(row);
-          }
-        });
+  private getFormattedDate(date) {
+    var year = date.getFullYear();
+
+    var month = (1 + date.getMonth()).toString();
+    month = month.length > 1 ? month : '0' + month;
+
+    var day = date.getDate().toString();
+    day = day.length > 1 ? day : '0' + day;
+
+    return day + '.' + month + '.' + year;
   }
 
   public print() {
-    this.pdf.setFont("Helvetica");
+    this.pdf.setFont('Helvetica');
     this.pdf.setFontSize(12);
 
     const recordsToPrint = [];
 
     this.selection.selected.forEach((selectedRecord) => {
-      this.dateFormated = moment(selectedRecord["date"]).format("DD.MM.YYYY");
-
       recordsToPrint.push([
-        this.dateFormated,
-        selectedRecord["description"],
-        selectedRecord["workingHours"],
-        selectedRecord["employee"],
+        this.getFormattedDate(selectedRecord.date.toDate()),
+        selectedRecord['description'],
+        selectedRecord['workingHours'],
+        selectedRecord['employee'],
       ]);
     });
 
-    console.log("Time Record", recordsToPrint);
+    console.log('Time Record', recordsToPrint);
 
-    const customerInfo = document.getElementById("customer-info");
-
+    const customerInfo = document.getElementById('customer-info');
 
     const date = customerInfo.children[0].children[0].childNodes[0].childNodes[1].textContent.trim();
     const customerName = customerInfo.children[0].children[0].childNodes[1].childNodes[1].textContent.trim();
@@ -267,54 +274,54 @@ export class OrderDetailComponent implements OnInit {
     const contactPerson = customerInfo.children[0].children[1].childNodes[1].childNodes[1].textContent.trim();
 
     this.pdf.text(
-      "Forstbetrieb Tschabi | Hochkreuthweg 3 | 87642 Trauchgau",
+      'Forstbetrieb Tschabi | Hochkreuthweg 3 | 87642 Trauchgau',
       12,
       20
     );
 
-    this.pdf.text("Datum: ", 12, 39);
+    this.pdf.text('Datum: ', 12, 39);
     this.pdf.text(date, 42, 359);
 
-    this.pdf.text("Kunde: ", 12, 46);
+    this.pdf.text('Kunde: ', 12, 46);
     this.pdf.text(customerName, 42, 46);
 
-    this.pdf.text("Ort: ", 12, 53);
+    this.pdf.text('Ort: ', 12, 53);
     this.pdf.text(location, 42, 53);
 
-    this.pdf.text("Einsatzleiter: ", 12, 60);
+    this.pdf.text('Einsatzleiter: ', 12, 60);
     this.pdf.text(contactPerson, 42, 60);
 
     this.pdf.autoTable({
-      head: [["Datum", "Beschreibung", "Arbeitsstunden", "Arbeiter"]],
+      head: [['Datum', 'Beschreibung', 'Arbeitsstunden', 'Arbeiter']],
       body: recordsToPrint,
       margin: { top: 78 },
-      pageBreak: "auto",
-      showHead: "everyPage",
-      showFoot: "everyPage",
+      pageBreak: 'auto',
+      showHead: 'everyPage',
+      showFoot: 'everyPage',
     });
 
     this.loadLogo();
   }
 
   private loadLogo() {
-    this.loadImage("assets/img/logo100px.png").then(
+    this.loadImage('assets/img/logo100px.png').then(
       (logo: HTMLImageElement) => {
-        this.pdf.addImage(logo, "PNG", 170, 17, 24, 24);
+        this.pdf.addImage(logo, 'PNG', 170, 17, 24, 24);
         this.loadFooterImage();
       }
     );
   }
 
   private loadFooterImage() {
-    this.loadImage("assets/img/letter_footer.png").then((logo) => {
-      this.pdf.addSvgAsImage("assets/letter_footer.svg", 200, 12, 200, 100);
+    this.loadImage('assets/img/letter_footer.png').then((logo) => {
+      this.pdf.addSvgAsImage('assets/letter_footer.svg', 200, 12, 200, 100);
       this.saveAsPdf();
     });
   }
 
   private saveAsPdf() {
-    const dateNow = moment().format("DD.MM.YYYY HH.MM");
-    const filename = "Regienstunden " + dateNow + ".pdf";
+    const dateNow = moment().format('DD.MM.YYYY HH.MM');
+    const filename = 'Regienstunden ' + dateNow + '.pdf';
     this.pdf.save(filename);
   }
 
