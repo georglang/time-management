32ada97b712e0d8409b7161d59eb425c463af9ae
Georg Lang <mail@georglang.com>
2020-11-11 21:11:16 +0100

    footer component removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,57 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { AppComponent } from "./app.component";
-import { ServiceWorkerModule } from "@angular/service-worker";
-import { environment } from "../environments/environment";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 // firebase modulese
-import { FirebaseOptionsToken } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { FirebaseOptionsToken } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 
 // own modules
-import { AppRoutingModule } from "./router/app.routing.module";
-import { ToastrModule } from "ngx-toastr";
+import { AppRoutingModule } from './router/app.routing.module';
+import { ToastrModule } from 'ngx-toastr';
 
 // pipes
-import { SortFormArrayByDate } from "./pipes/sort-form-array-by-date.pipe";
+import { SortFormArrayByDate } from './pipes/sort-form-array-by-date.pipe';
 
 // Angular Material
-import { MatButtonModule } from "@angular/material/button";
-import { MatCardModule } from "@angular/material/card";
-import { MatNativeDateModule, MAT_DATE_LOCALE } from "@angular/material/core";
-import { MatDatepickerModule } from "@angular/material/datepicker";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatListModule } from "@angular/material/list";
-import { MatMenuModule } from "@angular/material/menu";
-import { MatTableModule } from "@angular/material/table";
-import { MatTabsModule } from "@angular/material/tabs";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatNativeDateModule, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 // services
-import { FirestoreOrderService } from "./service/firestore-order-service/firestore-order.service";
-import { FirestoreRecordService } from "./service/firestore-record-service/firestore-record.service";
-import { IndexedDBService } from "./service/indexedDb-service/indexedDb.service";
-import { SynchronizeIdxDBWithFirebaseService } from "./service/synchronize-idxDb-with-firebase-service/synchronize-idxDb-with-firebase.service";
+import { FirestoreOrderService } from './service/firestore-order-service/firestore-order.service';
+import { FirestoreRecordService } from './service/firestore-record-service/firestore-record.service';
+import { IndexedDBService } from './service/indexedDb-service/indexedDb.service';
+import { SynchronizeIdxDBWithFirebaseService } from './service/synchronize-idxDb-with-firebase-service/synchronize-idxDb-with-firebase.service';
 
 // components
-import { OrderListComponent } from "./order-list/order-list.component";
-import { HeaderComponent } from "./header/header.component";
-import { Database } from "./database/Database";
-import { CreateOrderComponent } from "./create-order/create-order.component";
-import { OrderDetailComponent } from "./order-detail/order-detail.component";
-import { EditRecordComponent } from "./edit-record/edit-record.component";
-import { CreateRecordComponent } from "./create-record/create-record.component";
-import { ConfirmDeleteDialogComponent } from "./confirm-delete-dialog/confirm-delete-dialog.component";
-import { ConfirmPrintDialogComponent } from "./confirm-print-dialog/confirm-print-dialog.component";
+import { OrderListComponent } from './order-list/order-list.component';
+import { HeaderComponent } from './header/header.component';
+import { Database } from './database/Database';
+import { CreateOrderComponent } from './create-order/create-order.component';
+import { OrderDetailComponent } from './order-detail/order-detail.component';
+import { EditRecordComponent } from './edit-record/edit-record.component';
+import { CreateRecordComponent } from './create-record/create-record.component';
+import { ConfirmDeleteDialogComponent } from './confirm-delete-dialog/confirm-delete-dialog.component';
+import { ConfirmPrintDialogComponent } from './confirm-print-dialog/confirm-print-dialog.component';
 
-import { MatSelectModule } from "@angular/material/select";
-import { FooterComponent } from "./footer/footer.component";
-import { EditOrderComponent } from "./edit-order/edit-order.component";
+import { MatSelectModule } from '@angular/material/select';
+import { EditOrderComponent } from './edit-order/edit-order.component';
 
 @NgModule({
   declarations: [
@@ -66,9 +65,8 @@ import { EditOrderComponent } from "./edit-order/edit-order.component";
     CreateRecordComponent,
     ConfirmDeleteDialogComponent,
     ConfirmPrintDialogComponent,
-    FooterComponent,
     ConfirmPrintDialogComponent,
-    EditOrderComponent
+    EditOrderComponent,
   ],
   imports: [
     BrowserModule,
@@ -92,7 +90,7 @@ import { EditOrderComponent } from "./edit-order/edit-order.component";
     MatCheckboxModule,
     MatSelectModule,
     ToastrModule.forRoot(),
-    ServiceWorkerModule.register("ngsw-worker.js", {
+    ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
     }),
     AngularFirestoreModule,
@@ -110,7 +108,7 @@ import { EditOrderComponent } from "./edit-order/edit-order.component";
     },
     {
       provide: MAT_DATE_LOCALE,
-      useValue: "de-DE",
+      useValue: 'de-DE',
     },
   ],
   entryComponents: [ConfirmDeleteDialogComponent],
