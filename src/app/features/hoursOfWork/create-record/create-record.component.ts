b03c7a7474e58154ce9b4bd4aa93a59a8bd00b43
Georg Lang <mail@georglang.com>
2020-11-30 13:13:18 +0100

    create-record: tools added

--- src/app/features/hoursOfWork/create-record/create-record.component.ts
+++ src/app/features/hoursOfWork/create-record/create-record.component.ts
@@ -43,16 +43,40 @@ export class CreateRecordComponent implements OnInit {
   ];
   public tools = [
     {
-      value: 'Valtra',
-      viewValue: 'Valtra',
+      value: 'Mann',
+      viewValue: 'Mann',
+    },
+    {
+      value: 'Mann mit Motorsäge',
+      viewValue: 'Mann mit Motorsäge',
+    },
+    {
+      value: 'Seilschlepper',
+      viewValue: 'Seilschlepper',
+    },
+    {
+      value: 'Schlepper + Rückewagen',
+      viewValue: 'Schlepper + Rückewagen',
     },
     {
       value: 'HSM',
       viewValue: 'HSM',
     },
     {
-      value: 'Mann mit Motorsäge',
-      viewValue: 'Mann mit Motorsäge',
+      value: 'HSM + Woody',
+      viewValue: 'HSM + Woody',
+    },
+    {
+      value: 'Rückezug',
+      viewValue: 'Rückezug',
+    },
+    {
+      value: 'Rückezug + Winde + Bänder',
+      viewValue: 'Rückezug + Winde + Bänder',
+    },
+    {
+      value: 'Harvester',
+      viewValue: 'Harvester',
     }
   ];
 
