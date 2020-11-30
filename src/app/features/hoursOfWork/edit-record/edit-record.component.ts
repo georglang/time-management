84576cdd110422fc5e66b353bf68b5cd39e992d4
Georg Lang <mail@georglang.com>
2020-11-30 13:13:48 +0100

    edit-record: tools added

--- src/app/features/hoursOfWork/edit-record/edit-record.component.ts
+++ src/app/features/hoursOfWork/edit-record/edit-record.component.ts
@@ -52,17 +52,41 @@ export class EditRecordComponent implements OnInit {
 
   public tools = [
     {
-      value: 'Valtra',
-      viewValue: 'Valtra',
+      value: 'Mann',
+      viewValue: 'Mann',
+    },
+    {
+      value: 'Mann mit Motorsäge',
+      viewValue: 'Mann mit Motorsäge',
+    },
+    {
+      value: 'Seilschlepper',
+      viewValue: 'Seilschlepper',
+    },
+    {
+      value: 'Schlepper + Rückewagen',
+      viewValue: 'Schlepper + Rückewagen',
     },
     {
       value: 'HSM',
       viewValue: 'HSM',
     },
     {
-      value: 'Mann mit Motorsäge',
-      viewValue: 'Mann mit Motorsäge',
+      value: 'HSM + Woody',
+      viewValue: 'HSM + Woody',
     },
+    {
+      value: 'Rückezug',
+      viewValue: 'Rückezug',
+    },
+    {
+      value: 'Rückezug + Winde + Bänder',
+      viewValue: 'Rückezug + Winde + Bänder',
+    },
+    {
+      value: 'Harvester',
+      viewValue: 'Harvester',
+    }
   ];
 
   constructor(
