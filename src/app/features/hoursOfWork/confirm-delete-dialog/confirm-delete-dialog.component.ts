ed703a28700f3cca9248f23b884a52524ab55137
Georg Lang <mail@georglang.com>
2020-12-21 15:57:16 +0100

    renaming

--- src/app/features/hoursOfWork/confirm-delete-dialog/confirm-delete-dialog.component.ts
+++ src/app/features/hoursOfWork/confirm-delete-dialog/confirm-delete-dialog.component.ts
@@ -1,31 +1,31 @@
-import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
-  selector: "app-confirm-delete-dialog",
-  templateUrl: "./confirm-delete-dialog.component.html",
-  styleUrls: ["./confirm-delete-dialog.component.sass"],
+  selector: 'app-confirm-delete-dialog',
+  templateUrl: './confirm-delete-dialog.component.html',
+  styleUrls: ['./confirm-delete-dialog.component.sass'],
 })
 export class ConfirmDeleteDialogComponent implements OnInit {
   public modalTitle: string;
   public passwordCorrect: string;
   public hide = true;
-  public isPasswortCorrect = false;
+  public isPasswordCorrect = false;
 
   public passwordForm: FormGroup = new FormGroup({
-    password: new FormControl("", [Validators.required, Validators.min(3)]),
+    password: new FormControl('', [Validators.required, Validators.min(3)]),
   });
 
   get passwordInput() {
-    return this.passwordForm.get("password");
+    return this.passwordForm.get('password');
   }
 
   constructor() {}
 
   ngOnInit() {
-    this.passwordForm.controls["password"].valueChanges.subscribe((input) => {
-      if (input === "Löwenbräu") {
-        this.isPasswortCorrect = true;
+    this.passwordForm.controls['password'].valueChanges.subscribe((input) => {
+      if (input === 'Löwenbräu') {
+        this.isPasswordCorrect = true;
       }
     });
   }
