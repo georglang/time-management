20a8ad8a6643525e4bc282cf480960636535a33b
Georg Lang <mail@georglang.com>
2020-12-21 15:57:47 +0100

    logout button integrated in header

--- src/app/features/hoursOfWork/header/header.component.ts
+++ src/app/features/hoursOfWork/header/header.component.ts
@@ -1,20 +1,30 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AuthService } from '../../../core/auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.sass']
+  styleUrls: ['./header.component.sass'],
 })
 export class HeaderComponent implements OnInit {
   public navLinks = [
-    { path: '', label: '' }
+    { path: '', label: '' },
     // { path: '/create-order', label: 'Neuer Auftrag' },
     // { path: '/search', label: 'Suchen' }
   ];
+  public isLoggedIn = false;
 
   @Input() titleInput: string;
 
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn;
+
+    console.log('Is Logged In: ', this.isLoggedIn);
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
