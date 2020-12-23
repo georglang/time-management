fd2a939cf1d3eaf680f39560fcbb80cb04cc8eb7
Georg Lang <mail@georglang.com>
2020-12-23 08:47:32 +0100

    cleanup

--- src/app/core/guard/auth.guard.ts
+++ src/app/core/guard/auth.guard.ts
@@ -18,7 +18,6 @@ export class AuthGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
   ): Observable<boolean> | Promise<boolean> | boolean {
-    debugger;
     if (this.authService.isLoggedIn !== true) {
       this.router.navigate(['sign-in']);
     }
