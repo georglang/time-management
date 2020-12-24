8f70999f9e1b883110502925c7f144f45f23e579
Georg Lang <mail@georglang.com>
2020-12-24 06:27:42 +0100

    material-list: display fetched materials

--- src/app/features/hoursOfWork/material-list/material-list.component.ts
+++ src/app/features/hoursOfWork/material-list/material-list.component.ts
@@ -1,32 +1,43 @@
-// import { Component, OnInit } from '@angular/core';
-// import { FirestoreMaterialService } from '../services/firestore-material-service/firestore-material.service';
-// import { IMaterial } from '../data-classes/Material';
-// import { MatTableDataSource } from '@angular/material/table';
+import { Component, Input, OnInit } from '@angular/core';
+import { FirestoreMaterialService } from '../services/firestore-material-service/firestore-material.service';
+import { IMaterial } from '../data-classes/Material';
+import { MatTableDataSource } from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
 
-// @Component({
-//   selector: 'app-material-list',
-//   templateUrl: './material-list.component.html',
-//   styleUrls: ['./material-list.component.sass'],
-// })
-// export class MaterialListComponent implements OnInit {
-//   public displayedColumns = ['material', 'numberOfPieces'];
-//   public dataSource = new MatTableDataSource();
+@Component({
+  selector: 'app-material-list',
+  templateUrl: './material-list.component.html',
+  styleUrls: ['./material-list.component.sass'],
+})
+export class MaterialListComponent implements OnInit {
+  @Input() orderId: string;
+  public displayedColumns = ['date', 'material', 'amount', 'unit'];
+  public dataSource = new MatTableDataSource();
+  public hasMaterialsFound: boolean = false;
+  public selection = new SelectionModel<IMaterial>(true, []);
 
-//   constructor(private firestoreMaterialService: FirestoreMaterialService) {}
+  constructor(private firestoreMaterialService: FirestoreMaterialService) {}
 
-//   ngOnInit() {
-//     this.getMaterialsFromCloudDatabase();
-//   }
+  ngOnInit() {
+    this.getMaterialsFromCloudDatabase();
+  }
 
-//   public getMaterialsFromCloudDatabase(): void {
-//     if (this.firestoreMaterialService !== undefined) {
-//       this.firestoreMaterialService
-//         .getMaterialsFromMaterialsCollection()
-//         .then((materials: IMaterial[]) => {
-//           if (materials !== undefined) {
-//             this.dataSource = new MatTableDataSource();
-//           }
-//         });
-//     }
-//   }
-// }
+  public getMaterialsFromCloudDatabase(): void {
+    if (this.firestoreMaterialService !== undefined) {
+      this.firestoreMaterialService
+        .getMaterialsByOrderId(this.orderId)
+        .subscribe((materials: IMaterial[]) => {
+          if (materials.length > 0) {
+            this.setMaterialDataSource(materials);
+          } else {
+            this.hasMaterialsFound = false;
+          }
+        });
+    }
+  }
+
+  private setMaterialDataSource(materials: IMaterial[]) {
+    this.dataSource = new MatTableDataSource<IMaterial>(materials);
+    this.hasMaterialsFound = true;
+  }
+}
