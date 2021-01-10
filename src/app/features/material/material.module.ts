4f6b2abebf320d02ce5da6a175aeddb5def87afe
Georg Lang <mail@georglang.com>
2021-01-10 12:35:12 +0100

    feat(material.module): add Forms- and ReactiveForms module

--- src/app/features/material/material.module.ts
+++ src/app/features/material/material.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from '../../shared/shared.module';
+
 import { MaterialComponent } from './material.component';
 import { MaterialListComponent } from './material-list/material-list.component';
 import { EditMaterialComponent } from './edit-material/edit-material.component';
 import { CreateMaterialComponent } from './create-material/create-material.component';
-import { SharedModule } from '../../shared/shared.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, SharedModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedModule],
   declarations: [
     MaterialComponent,
     MaterialListComponent,
