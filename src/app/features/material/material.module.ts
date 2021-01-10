83ba5377237d054b705c18fe6996ad681877fd31
Georg Lang <mail@georglang.com>
2021-01-10 12:29:48 +0100

    feat(material.module): add FormModule

--- src/app/features/material/material.module.ts
+++ src/app/features/material/material.module.ts
@@ -5,9 +5,10 @@ import { MaterialListComponent } from './material-list/material-list.component';
 import { EditMaterialComponent } from './edit-material/edit-material.component';
 import { CreateMaterialComponent } from './create-material/create-material.component';
 import { SharedModule } from '../../shared/shared.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, FormsModule, SharedModule],
   declarations: [
     MaterialComponent,
     MaterialListComponent,
